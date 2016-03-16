35424b5134e4b4b61c1e6f74f6b0c087c890e3c6
Dipak Boyed <diboyed@microsoft.com>
2016-03-16 15:37:31 -0700

    Pascals Triangle

---
+++ EPIProblems/Arrays/Pascal'sTriangle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EPI.Arrays
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public static class Pascal_sTriangle
+	{
+		public static List<List<int>> GenerateFirstNRows(int n)
+		{
+			if (n < 1)
+			{
+				return null;
+			}
+
+			List<List<int>> pascalsTriangle = new List<List<int>>();
+			for (int i = 0; i < n; i++)
+			{
+				List<int> newRow = Enumerable.Repeat(1, i + 1).ToList();
+				int previousRow = pascalsTriangle.Count - 1;
+				for (int j = 1; j < i; j++)
+				{
+					newRow[j] = pascalsTriangle[previousRow][j - 1] + pascalsTriangle[previousRow][j];
+				}
+				pascalsTriangle.Add(newRow);
+			}
+
+			return pascalsTriangle;
+		}
+
+
+		public static List<int> GenerateNthRow(int n)
+		{
+			if (n < 1)
+			{
+				return null;
+			}
+			List<int> row = new List<int>();
+			for (int i = 0; i < n; i++)
+			{
+				List<int> newRow = Enumerable.Repeat(1, i + 1).ToList();
+				for (int j = 1; j < i; j++)
+				{
+					newRow[j] = row[j - 1] + row[j];
+				}
+				row = newRow;
+			}
+
+			return row;
+		}
+	}
+}
---
+++ EPIUnitTests/Arrays/Pascal_sTriangleUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using EPI.Arrays;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.Arrays
+{
+	[TestClass]
+	public class Pascal_sTriangleUnitTest
+	{
+
+		[TestMethod]
+		public void GenerateNRowsOfPascalsTriangle()
+		{
+			Pascal_sTriangle.GenerateFirstNRows(0).Should().BeNull();
+			Pascal_sTriangle.GenerateFirstNRows(1).ShouldBeEquivalentTo(new List<List<int>>() { new List<int>() { 1 } });
+			Pascal_sTriangle.GenerateFirstNRows(2).ShouldBeEquivalentTo(new List<List<int>>()
+			{
+				new List<int>() { 1 },
+				new List<int>() { 1, 1 },
+			});
+
+			Pascal_sTriangle.GenerateFirstNRows(5).ShouldBeEquivalentTo(new List<List<int>>()
+			{
+				new List<int>() { 1 },
+				new List<int>() { 1, 1 },
+				new List<int>() { 1, 2, 1 },
+				new List<int>() { 1, 3, 3, 1 },
+				new List<int>() { 1, 4, 6, 4, 1 },
+			});
+
+			Pascal_sTriangle.GenerateNthRow(5).Should().ContainInOrder(1, 4, 6, 4, 1);
+		}
+	}
+}
