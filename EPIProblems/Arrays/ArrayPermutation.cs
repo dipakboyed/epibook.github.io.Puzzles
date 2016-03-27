34640746a68a7b785ff2ce256e493d878ee48a57
Dipak Boyed <dipakboyed@hotmail.com>
2016-03-27 15:49:17 -0700

    array permutation

---
+++ EPIProblems/Arrays/ArrayPermutation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPI.Arrays
+{
+	/// <summary>
+	/// Given an array of n elements and a permutation P, apply P to A
+	/// using only constant storage.
+	/// Retunr the result in the original array itself
+	/// </summary>
+	/// <example> Permutation [3,2,1,0] on [a,b,c,d] will result in [d,c,b,a]</example>
+	public static class ArrayPermutation<T>
+	{
+		public static void ApplyPermutation(List<T> array, List<int> permutation) 
+		{
+			int size = permutation.Count;
+			// iterate each element in the array
+			for(int i = 0; i < array.Count; i++)
+			{
+				// if permutation has already been applied to the current position, it's Permutation value will be < 0
+				if (permutation[i] >= 0)
+				{
+					// keep shifting until the cycle is broken
+					int currentIndex = i;
+					T tempValue = array[currentIndex];
+					do
+					{
+						int nextIndex = permutation[currentIndex];
+						T nextValue = array[nextIndex];
+						array[nextIndex] = tempValue;
+
+						// mark as visited
+						permutation[currentIndex] -= size;
+
+						currentIndex = nextIndex;
+						tempValue = nextValue;
+					}
+					while (currentIndex != i);
+				}
+			}
+
+			// restore permutation (if we want to keep all input values unchanged)
+			for(int i = 0; i < permutation.Count; i++)
+			{
+				permutation[i] += size;
+			}
+		}
+	}
+}
---
+++ EPIUnitTests/Arrays/ArrayPermutationUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using EPI.Arrays;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.Arrays
+{
+	[TestClass]
+	public class ArrayPermutationUnitTest
+	{
+
+		[TestMethod]
+		public void ApplyPermutation()
+		{
+			List<char> array = new List<char> { 'a', 'b', 'c', 'd'};
+			ArrayPermutation<char>.ApplyPermutation(array, new List<int> { 3, 2, 1, 0 });
+			array.Should().ContainInOrder('d', 'c', 'b', 'a');
+
+			array = new List<char> { 'a', 'b', 'c', 'd' };
+			ArrayPermutation<char>.ApplyPermutation(array, new List<int> { 2, 3, 1, 0 });
+			array.Should().ContainInOrder('d', 'c', 'a', 'b');
+		}
+	}
+}
