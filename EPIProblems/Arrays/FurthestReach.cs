7e975b8638bb0025153eec28f0b38606e344b3bb
Dipak Boyed <diboyed@microsoft.com>
2016-03-30 15:26:54 -0700

    basic array cases

---
+++ EPIProblems/Arrays/FurthestReach.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EPI.Arrays
+{
+	/// <summary>
+	/// Write a function that takes an array of n integers, where A[i] denotes
+	/// the maximum you can advance from index i, and returns whether it is possible
+	/// to advance to the last index starting from the beginning of the array
+	/// </summary>
+	public static class FurthestReach
+	{
+		public static bool CanReachEnd(int[] array)
+		{
+			int furthestReach = 0;
+			for (int i = 0; i <= furthestReach && furthestReach < array.Length - 1; i++)
+			{
+				furthestReach = Math.Max(furthestReach, i + array[i]);
+			}
+
+			return furthestReach >= array.Length - 1;
+		}
+	}
+}
---
+++ EPIUnitTests/Arrays/FurthestReachUnitTest.cs
@@ -0,0 +1,19 @@
+﻿
+using EPI.Arrays;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.Arrays
+{
+	[TestClass]
+	public class FurthestReachUnitTest
+	{
+
+		[TestMethod]
+		public void CanReachFurthestArrayItem()
+		{
+			FurthestReach.CanReachEnd(new [] {3, 3, 1, 0, 2, 0, 1}).Should().BeTrue();
+			FurthestReach.CanReachEnd(new[] { 3,2,0,0,2,0,1}).Should().BeFalse();
+		}
+	}
+}
