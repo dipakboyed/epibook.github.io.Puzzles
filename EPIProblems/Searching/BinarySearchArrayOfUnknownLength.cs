3c5cd049f0ab5049ce96545533a02b0e79c9e41c
Dipak Boyed <dipakboyed@hotmail.com>
2016-05-21 11:53:29 -0700

    binary search in array of unknown length

---
+++ EPIProblems/Searching/BinarySearchArrayOfUnknownLength.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace EPI.Searching
+{
+	/// <summary>
+	/// DEesign an algorithm that takes a sorted array whose length is unknown and a key and returns an index of an array
+	/// element which matches the key. 
+	/// Assume that an out of bounds access throws an exception
+	/// </summary>
+	public static class BinarySearchArrayOfUnknownLength
+	{
+		public static int Search(int[] arrayOfUnknownLength, int key)
+		{
+			// since we dont know the length, lets determine the upper bound for our search
+			int i = 0;
+			while (true)
+			{
+				int upperBoundIndex = (1 << i) - 1;
+				try
+				{
+					if (arrayOfUnknownLength[upperBoundIndex] == key)
+					{
+						return upperBoundIndex;
+					}
+
+					if (arrayOfUnknownLength[upperBoundIndex] > key)
+					{
+						break; // we found our upper bound
+					}
+					++i;
+				}
+				catch (IndexOutOfRangeException)
+				{
+					// out of bounds, use last known upper bound
+					break;
+				}
+			}
+
+			// key was greater than index A[2^(i - 1)] but smaller than index A[2^(i) - 1]
+			int left = 1 << (i - 1);
+			int right = (1 << i) - 2;
+			while (left <= right)
+			{
+				int mid = left + (right - left) / 2;
+				try
+				{
+					if (arrayOfUnknownLength[mid] == key)
+					{
+						return mid;
+					}
+					else if (arrayOfUnknownLength[mid] > key)
+					{
+						right = mid - 1;
+					}
+					else // A[mid] < key
+					{
+						left = mid + 1;
+					}
+				}
+				catch (IndexOutOfRangeException)
+				{
+					right = mid - 1; // search the left part
+				}
+			}
+
+			return -1;
+		}
+	}
+}
---
+++ EPIUnitTests/Searching/BinarySearchArrayOfUnknownLengthUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using EPI.Searching;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.Searching
+{
+	[TestClass]
+	public class BinarySearchArrayOfUnknownLengthUnitTest
+	{
+		private Random rand;
+		private int[] largeArray;
+
+		public BinarySearchArrayOfUnknownLengthUnitTest()
+		{
+			rand = new Random();
+			List<int> numbers = new List<int>();
+			for (int i =0; i < 10000; i++)
+			{
+				numbers.Add(rand.Next());
+			}
+			numbers.Sort();
+			largeArray = numbers.ToArray();
+		}
+
+		[TestMethod]
+		public void BinarySearchElementInArrayOfUnknownLength()
+		{
+			int index = rand.Next(10000);
+			int valueToSearch = largeArray[index];
+			BinarySearchArrayOfUnknownLength.Search(largeArray, valueToSearch).Should().Be(index);
+		}
+	}
+}
