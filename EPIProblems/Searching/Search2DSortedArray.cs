718e7e8c6b1265cc1e70a6a76be5002860fa5fb0
Dipak Boyed <diboyed@microsoft.com>
2016-05-21 18:45:55 -0700

    Search a 2D sorted array

---
+++ EPIProblems/Searching/Search2DSortedArray.cs
@@ -0,0 +1,34 @@
+﻿ namespace EPI.Searching
+{
+	/// <summary>
+	/// Design an algorithm that checks whether a number appears in a 2D sorted array or not.
+	/// A 2D array is considered sorted if all rows and columns are sorted.
+	/// </summary>
+	public static class Search2DSortedArray
+	{
+		public static bool Search(int[,] array, int valueToSearch)
+		{
+			int row = array.GetLength(0) - 1;
+			int column = 0;
+			while (row >= 0 && column < array.GetLength(1))
+			{
+				if (array[row, column] == valueToSearch)
+				{
+					return true;
+				}
+				else if (array[row, column] < valueToSearch)
+				{
+					// value is not present in current column, increment column
+					column++;
+				}
+				else
+				{
+					// value might be in current column, decrement row
+					row--;
+				}
+			}
+
+			return false;
+		}
+	}
+}
---
+++ EPIUnitTests/Searching/Search2DSortedArrayUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using EPI.Searching;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.Searching
+{
+	[TestClass]
+	public class Search2DSortedArrayUnitTest
+	{
+		[TestMethod]
+		public void Search2DSorted()
+		{
+			int[,] array = new[,]
+			{
+				{-1, 2,  4,  4,  6 },
+				{ 1, 5,  5,  9,  21},
+				{ 3, 6,  6,  9,  22},
+				{ 3, 6,  8,  10, 24},
+				{ 6, 8,  9,  12, 25},
+				{ 8, 10, 12, 13, 40}
+			};
+			Search2DSortedArray.Search(array, 8).Should().BeTrue();
+			Search2DSortedArray.Search(array, 5).Should().BeTrue();
+			Search2DSortedArray.Search(array, 7).Should().BeFalse();
+		}
+	}
+}
