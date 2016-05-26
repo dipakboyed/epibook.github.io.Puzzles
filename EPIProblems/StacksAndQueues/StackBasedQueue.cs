40a6f7cde2c79cd3224f7dab8e07f3983663f060
Dipak Boyed <dipakboyed@hotmail.com>
2016-05-25 20:26:02 -0700

    Stack based queue

---
+++ EPIProblems/StacksAndQueues/StackBasedQueue.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace EPI.StacksAndQueues
+{
+	/// <summary>
+	/// Implement a queue based on a Stack library
+	/// </summary>
+	public class StackBasedQueue<T>
+	{
+		private Stack<T> stack1, stack2;
+
+		public StackBasedQueue()
+		{
+			stack1 = new Stack<T>();
+			stack2 = new Stack<T>();
+		}
+
+		public void Enqueue(T entry)
+		{
+			stack1.Push(entry);
+		}
+
+		public T Dequeue()
+		{
+			if (stack2.Count == 0)
+			{
+				// Transfer all element from first stack to the second
+				// but in reverse order
+				while (stack1.Count != 0)
+				{
+					stack2.Push(stack1.Pop());
+				}
+			}
+
+			return stack2.Pop();
+		}
+
+		public T Peek()
+		{
+			if (stack2.Count == 0)
+			{
+				// Transfer all element from first stack to the second
+				// but in reverse order
+				while (stack1.Count != 0)
+				{
+					stack2.Push(stack1.Pop());
+				}
+			}
+
+			return stack2.Peek();
+		}
+
+		public void	Clear()
+		{
+			stack1.Clear();
+			stack2.Clear();
+		}
+	}
+}
---
+++ EPIUnitTests/StacksAndQueues/StackBasedQueueUnitTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using EPI.StacksAndQueues;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.StacksAndQueues
+{
+	[TestClass]
+	public class StackBasedQueueUnitTest
+	{
+		[TestMethod]
+		public void StackBasedQueueEnqueAndPeek()
+		{
+			StackBasedQueue<int> stackBasedQueue = new StackBasedQueue<int>();
+			stackBasedQueue.Enqueue(1);
+			stackBasedQueue.Enqueue(2);
+			stackBasedQueue.Enqueue(3);
+			stackBasedQueue.Enqueue(4);
+			stackBasedQueue.Peek().Should().Be(1);
+			//peek again and ensure we didnt dequeue
+			stackBasedQueue.Peek().Should().Be(1);
+		}
+
+		[TestMethod]
+		public void StackBasedQueueEnqueAndDequeAndPeek()
+		{
+			StackBasedQueue<int> stackBasedQueue = new StackBasedQueue<int>();
+			stackBasedQueue.Enqueue(1);
+			stackBasedQueue.Enqueue(2);
+			stackBasedQueue.Enqueue(3);
+			stackBasedQueue.Enqueue(4);
+
+			stackBasedQueue.Dequeue().Should().Be(1);
+			stackBasedQueue.Peek().Should().Be(2);
+
+			stackBasedQueue.Dequeue().Should().Be(2);
+			stackBasedQueue.Enqueue(5);
+			stackBasedQueue.Enqueue(6);
+
+			stackBasedQueue.Dequeue().Should().Be(3);
+			stackBasedQueue.Dequeue().Should().Be(4);
+			stackBasedQueue.Dequeue().Should().Be(5);
+			stackBasedQueue.Dequeue().Should().Be(6);
+			Action action = () => 
+			{
+				stackBasedQueue.Dequeue();
+			};
+			action.ShouldThrow<InvalidOperationException>();
+		}
+
+		[TestMethod]
+		public void StackBasedQueueClear()
+		{
+			StackBasedQueue<int> stackBasedQueue = new StackBasedQueue<int>();
+			stackBasedQueue.Enqueue(1);
+			stackBasedQueue.Enqueue(2);
+			stackBasedQueue.Clear();
+			Action action = () =>
+			{
+				stackBasedQueue.Dequeue();
+			};
+			action.ShouldThrow<InvalidOperationException>();
+		}
+	}
+}
