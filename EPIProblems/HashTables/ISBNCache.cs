c89f33d56f6bb3d7b82b6a4b8551f7c959e97eba
Dipak Boyed <dipakboyed@hotmail.com>
2016-05-28 14:01:54 -0700

    ISBN Cache

---
+++ EPIProblems/HashTables/ISBNCache.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EPI.HashTables
+{
+	/// <summary>
+	/// Implement a cache for looking up prices of books identified by their ISBN number.
+	/// You should support Lookup, Insert, Erase methods and use the least recently used (LRU)
+	/// cache eviction policy
+	/// </summary>
+	public class ISBNCache
+	{
+		private const int cacheCapacity = 5;
+
+		private Dictionary<int, Tuple<int, LinkedListNode<int>>> isbnTable;
+		private LinkedList<int> lruCache;
+
+		public ISBNCache()
+		{
+			isbnTable = new Dictionary<int, Tuple<int, LinkedListNode<int>>>();
+			lruCache = new LinkedList<int>();
+		}
+
+		private void MoveItemToFrontOfLruCache(int itemISBN, LinkedListNode<int> cacheNode)
+		{
+			lruCache.Remove(cacheNode);
+			lruCache.AddFirst(itemISBN);
+		}
+
+		public int Lookup(int isbn)
+		{
+			if (isbnTable.ContainsKey(isbn))
+			{
+				//already exists, move to front in LRU cache
+				MoveItemToFrontOfLruCache(isbn, isbnTable[isbn].Item2);
+				return isbnTable[isbn].Item1;
+			}
+			else
+			{
+				return -1;
+			}
+		}
+
+		public void Insert(int isbn, int price)
+		{
+			if (isbnTable.ContainsKey(isbn))
+			{
+				//already exists, move to front in LRU cache
+				MoveItemToFrontOfLruCache(isbn, isbnTable[isbn].Item2);
+			}
+			else
+			{
+				if (lruCache.Count == cacheCapacity)
+				{
+					// evict LRU item
+					isbnTable.Remove(lruCache.Last.Value);
+					lruCache.RemoveLast();
+				}
+				LinkedListNode<int> node = lruCache.AddFirst(isbn);
+				isbnTable.Add(isbn, new Tuple<int, LinkedListNode<int>>(price, node));
+			}
+		}
+
+		public bool Erase(int isbn)
+		{
+			if (isbnTable.ContainsKey(isbn))
+			{
+				lruCache.Remove(isbnTable[isbn].Item2);
+				isbnTable.Remove(isbn);
+				return true;
+			}
+			return false;
+		}
+	}
+}
---
+++ EPIUnitTests/HashTables/ISBNCacheUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using EPI.HashTables;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.HashTables
+{
+	[TestClass]
+	public class ISBNCacheUnitTest
+	{
+		[TestMethod]
+		public void ISBNLruCache()
+		{
+			ISBNCache cache = new ISBNCache();
+			cache.Lookup(1).Should().Be(-1);
+			cache.Insert(1, 10);
+			cache.Insert(2, 20);
+			cache.Insert(3, 30);
+			cache.Insert(4, 40);
+			cache.Insert(5, 50);
+			cache.Insert(6, 60);
+			cache.Lookup(1).Should().Be(-1, because: "LRU item was evicted");
+			cache.Lookup(2).Should().Be(20, because: "non LRU item is found");
+			cache.Erase(5).Should().BeTrue();
+			cache.Insert(7, 70);
+			cache.Insert(8, 80);
+			cache.Insert(3, 30);
+			cache.Lookup(4).Should().Be(-1, because: " LRU item was evicted");
+		}
+	}
+}
