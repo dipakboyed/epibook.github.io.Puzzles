0bbc6dea85e8a84ac3213d2e343f8a0eb9ea8f7a
Dipak Boyed <dipakboyed@hotmail.com>
2016-05-27 23:36:21 -0700

    Circular queue

---
+++ EPIProblems/StacksAndQueues/CircularQueue.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace EPI.StacksAndQueues
+{
+	/// <summary>
+	/// A queue can be implemented using an array and a beginning (head) and end (tail)
+	/// indices. This data structure is often referred to as a circular queue.
+	/// Implement a circular queue class containing a constructor that takes the capacity,
+	/// enqueue, dequeue and size function and implements dynamic resizing.
+	/// </summary>
+	public class CircularQueue<T>
+	{
+		private T[] array;
+		private int head;
+		private int tail;
+		private int size;
+		private const int resizeFactor = 2;
+
+		public CircularQueue(int capacity)
+		{
+			array = new T[capacity];
+			head = 0;
+			tail = 0;
+			size = 0;
+		}
+
+		public int Size
+		{
+			get
+			{
+				return size;
+			}
+		}
+
+		public void Enqueue(T item)
+		{
+			if (Size == array.Length)
+			{
+				// array capacity is full, resize
+				T[] newArray = new T[array.Length * resizeFactor];
+				for (int i = 0; i < Size; i++)
+				{
+					newArray[i] = array[(head + i) % array.Length];
+				}
+				head = 0;
+				tail = Size;
+				array = newArray;
+			}
+			array[tail] = item;
+			tail = (tail + 1) % array.Length;
+			++size;
+		}
+
+		public T Dequeue()
+		{
+			if (size <= 0)
+			{
+				throw new InvalidOperationException("Queue is empty");
+			}
+			// return the item at head and increment the head
+			int currentHead = head;
+			head = (head + 1) % array.Length;
+			--size;
+			return array[currentHead];
+		}
+	}
+}
---
+++ EPIUnitTests/StacksAndQueues/CircularQueueUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using EPI.StacksAndQueues;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.StacksAndQueues
+{
+	[TestClass]
+	public class CircularQueueUnitTest
+	{
+		[TestMethod]
+		public void CircularQueueTest()
+		{
+			CircularQueue<char> circularQueue = new CircularQueue<char>(5);
+			circularQueue.Enqueue('A');
+			circularQueue.Enqueue('B');
+			circularQueue.Enqueue('C');
+			circularQueue.Enqueue('D');
+			circularQueue.Enqueue('E');
+			circularQueue.Size.Should().Be(5);
+			circularQueue.Dequeue().Should().Be('A');
+			circularQueue.Size.Should().Be(4);
+			circularQueue.Enqueue('F');
+			circularQueue.Size.Should().Be(5);
+			circularQueue.Dequeue().Should().Be('B');
+			circularQueue.Dequeue().Should().Be('C');
+			circularQueue.Dequeue().Should().Be('D');
+			circularQueue.Dequeue().Should().Be('E');
+			circularQueue.Dequeue().Should().Be('F');
+			circularQueue.Size.Should().Be(0);
+
+			Action action = () =>
+			{
+				circularQueue.Dequeue();
+			};
+			action.ShouldThrow<InvalidOperationException>();
+
+			//resize
+			circularQueue.Enqueue('A');
+			circularQueue.Enqueue('B');
+			circularQueue.Enqueue('C');
+			circularQueue.Enqueue('D');
+			circularQueue.Enqueue('E');
+			circularQueue.Dequeue();
+			circularQueue.Enqueue('F');
+			circularQueue.Enqueue('G');
+			circularQueue.Enqueue('H');
+			circularQueue.Enqueue('I');
+			circularQueue.Enqueue('J');
+			circularQueue.Size.Should().Be(9);
+		}
+	}
+}
