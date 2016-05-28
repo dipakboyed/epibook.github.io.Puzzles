3a2eaf38d7ae8c727b29238ec69eb7cafe64da23
Dipak Boyed <dipakboyed@hotmail.com>
2016-05-28 12:28:11 -0700

    find anagrams in words

---
+++ EPIProblems/HashTables/PartitionAnagrams.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace EPI.HashTables
+{
+
+	/// <summary>
+	/// Write a function that takes as input a set of words and returns groups of anagrams
+	/// for those words
+	/// </summary>
+	/// <example>
+	/// Input: { "debitcard", "elvis", "silent", "badcredit", "lives", "freedom", listen", "levis"}
+	/// returns 3 anagram groups: {{"debitcard", "badcredit"}, {"elvis", "lives", "levis"}, {"silent", "listen"}}
+	/// </example>
+	public static class PartitionAnagrams
+	{
+		public static List<List<string>> FindAnagrams(string[] words)
+		{
+			Dictionary<string, List<string>> sortedWordsToAnagrams = new Dictionary<string, List<string>>();
+			//Iterate all words, sort them and add to the dictionary
+			// O(nmlogm) where n is word count and m is largest word length
+			foreach (string word in words)
+			{
+				string sortedWord = Sort(word);
+				if (!sortedWordsToAnagrams.ContainsKey(sortedWord))
+				{
+					sortedWordsToAnagrams.Add(sortedWord, new List<string>());
+				}
+				sortedWordsToAnagrams[sortedWord].Add(word);
+			}
+
+			// look for any values in the dictionary with count >= 2
+			List<List<string>> anagramGroups = new List<List<string>>();
+			foreach (var item in sortedWordsToAnagrams.Values)
+			{
+				if (item.Count >= 2)
+				{
+					anagramGroups.Add(item);
+				}
+			}
+			return anagramGroups;
+		}
+
+		private static string Sort(string word)
+		{
+			List<char> charList = word.ToList<char>();
+			charList.Sort();
+			return new string(charList.ToArray());
+		}
+	}
+}
---
+++ EPIUnitTests/HashTables/PartitionAnagramsUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using EPI.HashTables;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.HashTables
+{
+	[TestClass]
+	public class PartitionAnagramsUnitTest
+	{
+		[TestMethod]
+		public void FindAnagramGroups()
+		{
+			string[] words = new[] { "debitcard", "elvis", "silent", "badcredit", "lives", "freedom", "listen", "levis" };
+			PartitionAnagrams.FindAnagrams(words).ShouldBeEquivalentTo(new List<List<string>>()
+			{
+				new List<string> { "debitcard", "badcredit"},
+				new List<string> { "elvis", "lives", "levis"},
+				new List<string> { "silent", "listen"}
+			});
+
+		}
+	}
+}
