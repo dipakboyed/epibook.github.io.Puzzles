9a7ba24a2e9a616c2104287948163d6071947055
Dipak Boyed <dipakboyed@hotmail.com>
2016-05-27 22:21:40 -0700

    Shortest straight line

---
+++ EPIUnitTests/StacksAndQueues/ShortestStraightLineUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using EPI.StacksAndQueues;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.StacksAndQueues
+{
+	[TestClass]
+	public class ShortestStraightLineUnitTest
+	{
+		[TestMethod]
+		public void ShortestStraightLineSequence()
+		{
+			ShortestStraightLine.GetShortestStraightLine(7).ShouldBeEquivalentTo(new List<int> { 1, 2, 3, 4, 7});
+			ShortestStraightLine.GetShortestStraightLine(1).ShouldBeEquivalentTo(new List<int> { 1 });
+			ShortestStraightLine.GetShortestStraightLine(10).ShouldBeEquivalentTo(new List<int> { 1, 2, 3, 5, 10 });
+			ShortestStraightLine.GetShortestStraightLine(15).ShouldBeEquivalentTo(new List<int> { 1, 2, 3, 5, 10, 15 });
+		}
+	}
+}
