51167f0121a9e7c219568e3e53fc4f2898de91e0
Dipak Boyed <dipakboyed@hotmail.com>
2016-05-29 10:34:01 -0700

    nearest repeated words in a paragraph

---
+++ EPIProblems/HashTables/NearestRepeatedWords.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EPI.HashTables
+{
+	/// <summary>
+	/// People don't like reading text in which a word is repeated multiples times.
+	/// Write a function which takes as input an array and finds the distance between a closest
+	/// pair of equal entries.
+	/// </summary>
+	/// <exmaple>
+	/// If array = {"All", "work", "and", "no", "play", "makes", "for", "no", "work", "no", "fun", "and", "no", "results"}
+	/// then the 2nd and 3rd occurence of "no" is the closest pair.
+	/// </exmaple>
+	public static class NearestRepeatedWords
+	{
+		public static Tuple<int, int> FindNearestRepeatedEntries(string[] array)
+		{
+			Dictionary<string, int> wordToLatestIndex = new Dictionary<string, int>();
+			Tuple<int, int> closestPairSoFar = new Tuple<int, int>(0, Int32.MaxValue);
+
+			for (int i =0;i < array.Length; i++)
+			{
+				if (!wordToLatestIndex.ContainsKey(array[i]))
+				{
+					wordToLatestIndex.Add(array[i], i);
+				}
+				else
+				{
+					if (closestPairSoFar.Item2 - closestPairSoFar.Item1 > i - wordToLatestIndex[array[i]])
+					{
+						closestPairSoFar = new Tuple<int, int>(wordToLatestIndex[array[i]], i);
+					}
+					wordToLatestIndex[array[i]] = i;
+				}
+			}
+
+			return closestPairSoFar;
+		}
+	}
+}
---
+++ EPIUnitTests/HashTables/NearestRepeatedWordsUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using EPI.HashTables;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.HashTables
+{
+	[TestClass]
+	public class NearestRepeatedWordsUnitTest
+	{
+		[TestMethod]
+		public void FindNearestRepeatedWords()
+		{
+			NearestRepeatedWords.FindNearestRepeatedEntries(new[]
+				{"All", "work", "and", "no", "play", "makes", "for", "no", "work", "no", "fun", "and", "no", "results" }
+			).ShouldBeEquivalentTo(new Tuple<int, int>(7, 9));
+		}
+	}
+}
