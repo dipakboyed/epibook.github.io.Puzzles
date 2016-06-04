e4c0407ce5121275c2064c6233d15f707664c7b2
Dipak Boyed <dipakboyed@hotmail.com>
2016-06-04 09:35:37 -0700

    in place merge sorting

---
+++ EPIProblems/Sorting/InPlaceMergeSort.cs
@@ -0,0 +1,46 @@
+﻿namespace EPI.Sorting
+{
+	/// <summary>
+	/// Write a function that takes two input sorted arrays and updates the first to the combined
+	/// entries of the two arrays in sorted order.
+	/// Assume the first array has enough empty entries at the end to hold the final result
+	/// </summary>
+	/// <example>
+	/// {5,13,17, , , , , } and {3,7,11,19} updates the first array to {3,5,7,11,13,19, }
+	/// </example>
+	public static class InPlaceMergeSort
+	{
+		public static void Sort(int?[] A, int?[] B)
+		{
+			// calculate non-empty entries in A
+			// empty entries are denoted as null
+			int i = 0;
+			while (A[i] != null)
+			{
+				i++;
+			}
+			int j = B.Length;
+			int writeIndex = i + j - 1;
+			--i;
+			--j;
+			while (i >= 0 && j >= 0)
+			{
+				if(A[i] > B[j])
+				{
+					A[writeIndex--] = A[i--];
+				}
+				else
+				{
+					A[writeIndex--] = B[j--];
+				}
+			}
+
+			// if A has remaining items, they are already sorted in the right place
+			// add any remaining items in B
+			while (j >= 0)
+			{
+				A[writeIndex--] = B[j--];
+			}
+		}
+	}
+}
---
+++ EPIUnitTests/Sorting/InPlaceMergeSortUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using EPI.Sorting;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.Sorting
+{
+	[TestClass]
+	public class InPlaceMergeSortUnitTest
+	{
+		[TestMethod]
+		public void InPlaceMerge()
+		{
+			int?[] A = new int?[] { 5, 13, 17, null, null, null, null, null };
+			int?[] B = new int?[] { 3, 7, 11, 19 };
+			InPlaceMergeSort.Sort(A, B);
+			A.ShouldBeEquivalentTo(new int?[] { 3, 5, 7, 11, 13, 17, 19, null });
+		}
+	}
+}
