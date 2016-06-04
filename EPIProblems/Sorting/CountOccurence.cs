6d7940c144d2bf0bd2e0ce2f8e33e4bfe341190d
Dipak Boyed <dipakboyed@hotmail.com>
2016-06-04 10:18:54 -0700

    count occurences

---
+++ EPIProblems/Sorting/CountOccurence.cs
@@ -0,0 +1,33 @@
+﻿namespace EPI.Sorting
+{
+	/// <summary>
+	/// Give a string, print in alphabetical order each character that appears in the string and the number of times in appears
+	/// </summary>
+	/// <example>
+	/// "bcdacebe" outputs: "(a,1)(b,2)(c,2)(d,1)(e,2)"
+	/// </example>
+	public static class CountOccurence
+	{
+		public static string CountCharacterOccurenceInSortOrder(string input)
+		{
+			char[] array = input.ToCharArray();
+			QuickSort<char>.Sort(array);
+
+			string result = string.Empty;
+			int currentCharCount = 1;
+			for (int i = 1; i < array.Length; i++)
+			{
+				if (array[i] == array[i-1])
+				{
+					++currentCharCount;
+				}
+				else
+				{
+					result += "(" + array[i - 1] + "," + currentCharCount + ")";
+					currentCharCount = 1;
+				}
+			}
+			return result;
+		}
+	}
+}
---
+++ EPIUnitTests/Sorting/CountOccurenceUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using EPI.Sorting;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.Sorting
+{
+	[TestClass]
+	public class CountOccurenceUnitTest
+	{
+		[TestMethod]
+		public void CountCharacterOccurence()
+		{
+			CountOccurence.CountCharacterOccurenceInSortOrder("bcdacebe").Should().Be("(a,1)(b,2)(c,2)(d,1)(e,2)");
+		}
+	}
+}
