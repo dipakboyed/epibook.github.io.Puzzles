46e9185cf44bbfe6c0d5da3bcfaa9cd19e2872e0
Dipak Boyed <dipakboyed@hotmail.com>
2016-06-04 11:32:46 -0700

    Render Calendar: find max concurrent meetings

---
+++ EPIProblems/Sorting/RenderCalendar.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPI.Sorting
+{
+	/// <summary>
+	/// Write a program that takes a set of calendar events (with start and end times) and determines
+	/// the maximum number of calendart events taking place concurrently
+	/// </summary>
+	public static class RenderCalendar
+	{
+		public struct CalendarEvent
+		{
+			public DateTime Start;
+			public DateTime Finish;
+
+			public CalendarEvent(DateTime start, DateTime finish)
+			{
+				Start = start;
+				Finish = finish;
+			}
+		};
+
+		private class Entry : IComparable
+		{
+			public DateTime Time;
+			public bool IsEventStart;
+
+			public Entry(DateTime time, bool isStart)
+			{
+				Time = time;
+				IsEventStart = isStart;
+			}
+
+			public int CompareTo(object obj)
+			{
+				Entry e = obj as Entry;
+				if (e != null)
+				{
+					return (Time != e.Time) ? Time.CompareTo(e.Time) : IsEventStart.CompareTo(e.IsEventStart);
+				}
+				throw new InvalidOperationException();
+			}
+		}
+
+		public static int FindMaxConcurrentEvents(List<CalendarEvent> events)
+		{
+			// foreach calendar event add a time entry for its start and end time
+			Entry[] entries = new Entry[events.Count * 2];
+			for (int i = 0; i < events.Count; i++)
+			{
+				entries[2 * i]	 = new Entry(events[i].Start, true);
+				entries[2 * i + 1] = new Entry(events[i].Finish, false);
+			}
+
+			// sort all time entries
+			QuickSort<Entry>.Sort(entries);
+
+			int maxConcurrentEntries = 0;
+			int currentConcurrentEntriesCount = 0;
+			foreach (Entry entry in entries)
+			{
+				if (entry.IsEventStart)
+				{
+					++currentConcurrentEntriesCount;
+					if (maxConcurrentEntries < currentConcurrentEntriesCount)
+					{
+						maxConcurrentEntries = currentConcurrentEntriesCount;
+					}
+				}
+				else
+				{
+					--currentConcurrentEntriesCount;
+				}
+			}
+			return maxConcurrentEntries;
+		}
+	}
+}
---
+++ EPIUnitTests/Sorting/RenderCalendarUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using EPI.Sorting;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.Sorting
+{
+	[TestClass]
+	public class RenderCalendarUnitTest
+	{
+		[TestMethod]
+		public void FindMaxConcurrentCalendarEvents()
+		{
+			List<RenderCalendar.CalendarEvent> calendar = new List<RenderCalendar.CalendarEvent>()
+			{
+				new RenderCalendar.CalendarEvent(new DateTime(2016,6,4,1,0,0), new DateTime(2016,6,4,5,0,0)),
+				new RenderCalendar.CalendarEvent(new DateTime(2016,6,4,6,0,0), new DateTime(2016,6,4,10,0,0)),
+				new RenderCalendar.CalendarEvent(new DateTime(2016,6,4,11,0,0), new DateTime(2016,6,4,13,0,0)),
+				new RenderCalendar.CalendarEvent(new DateTime(2016,6,4,14,0,0), new DateTime(2016,6,4,15,0,0)),
+				new RenderCalendar.CalendarEvent(new DateTime(2016,6,4,2,0,0), new DateTime(2016,6,4,7,0,0)),
+				new RenderCalendar.CalendarEvent(new DateTime(2016,6,4,8,0,0), new DateTime(2016,6,4,9,0,0)),
+				new RenderCalendar.CalendarEvent(new DateTime(2016,6,4,12,0,0), new DateTime(2016,6,4,15,0,0)),
+				new RenderCalendar.CalendarEvent(new DateTime(2016,6,4,4,0,0), new DateTime(2016,6,4,5,0,0)),
+				new RenderCalendar.CalendarEvent(new DateTime(2016,6,4,9,0,0), new DateTime(2016,6,4,17,0,0))
+			};
+			RenderCalendar.FindMaxConcurrentEvents(calendar).Should().Be(3);
+		}
+	}
+}
