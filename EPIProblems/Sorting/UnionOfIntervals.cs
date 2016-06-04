e8f6baa32a9d5a0768742f31c789573946ab33cc
Dipak Boyed <dipakboyed@hotmail.com>
2016-06-04 14:57:53 -0700

    find union of intervals

---
+++ EPIProblems/Sorting/UnionOfIntervals.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPI.Sorting
+{
+	/// <summary>
+	/// Design an algorithm that takes as input a set of intervals and outputs their union expressed as a set of disjoint intervals.
+	/// </summary>
+	/// <example>
+	/// {} => open interval, [] => closed interval
+	/// {0,3},[1,1],[3,4},[2,4],[5,7},[7,8},[8,11},{9,11], [12,14],{12,16],{13,15},{16,17} returns
+	/// {0,4],[5,11],[12,17}
+	/// </example>
+	public static class UnionOfIntervals
+	{
+		public struct Entry
+		{
+			public int value;
+			public bool isOpenInterval;
+		}
+
+		public struct Interval
+		{
+			public Entry left;
+			public Entry right;
+		}
+
+		private class EntryType : IComparable
+		{
+			public Entry entry;
+			public bool isRightInterval;
+
+			public int CompareTo(object obj)
+			{
+				EntryType entryType = obj as EntryType;
+				if (entryType != null)
+				{
+					return (entry.value != entryType.entry.value) ?
+						entry.value.CompareTo(entryType.entry.value) : // first sort entry's with lowest value
+						(isRightInterval != entryType.isRightInterval) ?  
+							isRightInterval.CompareTo(entryType.isRightInterval) :  // then sort by entry type which are left 
+							(isRightInterval) ? (!entry.isOpenInterval).CompareTo(!entryType.entry.isOpenInterval)  : entry.isOpenInterval.CompareTo(entryType.entry.isOpenInterval); //then sort by closed interval entry 
+				}
+				throw new InvalidOperationException();
+			}
+		}
+
+		public static List<Interval> FindUnionOfIntervals(List<Interval> intervals)
+		{
+			List<Interval> result = new List<Interval>();
+			EntryType[] entries = new EntryType[intervals.Count * 2];
+			for (int i = 0; i < intervals.Count; i++)
+			{
+				entries[2 * i] = new EntryType() { entry = intervals[i].left, isRightInterval = false };
+				entries[2*i + 1] = new EntryType() { entry = intervals[i].right, isRightInterval = true };
+			}
+
+			QuickSort<EntryType>.Sort(entries);
+			Stack<EntryType> stackedEntries = new Stack<EntryType>();
+			foreach (EntryType entryType in entries)
+			{
+				if (!entryType.isRightInterval)
+				{
+					stackedEntries.Push(entryType);
+				}
+				else
+				{
+					EntryType oldestEntryInSet = stackedEntries.Pop();
+					if (stackedEntries.Count == 0) // set is complete
+					{
+						result.Add(new Interval()
+						{
+							left = oldestEntryInSet.entry,
+							right = entryType.entry
+						});
+					}
+				}
+			}
+			return result;
+		}
+	}
+}
---
+++ EPIUnitTests/Sorting/UnionOfIntervalsUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using EPI.Sorting;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.Sorting
+{
+	[TestClass]
+	public class UnionOfIntervalsUnitTest
+	{
+		[TestMethod]
+		public void FindUnionOfDisjointIntervals()
+		{
+			List<UnionOfIntervals.Interval> intervals = new List<UnionOfIntervals.Interval>()
+			{
+				new UnionOfIntervals.Interval() {left = new UnionOfIntervals.Entry() {value = 0, isOpenInterval = true}, right = new UnionOfIntervals.Entry() {value = 3, isOpenInterval = true }},
+				new UnionOfIntervals.Interval() {left = new UnionOfIntervals.Entry() {value = 1, isOpenInterval = false}, right = new UnionOfIntervals.Entry() {value = 1, isOpenInterval = false }},
+				new UnionOfIntervals.Interval() {left = new UnionOfIntervals.Entry() {value = 3, isOpenInterval = false}, right = new UnionOfIntervals.Entry() {value = 4, isOpenInterval = false }},
+				new UnionOfIntervals.Interval() {left = new UnionOfIntervals.Entry() {value = 2, isOpenInterval = false}, right = new UnionOfIntervals.Entry() {value = 4, isOpenInterval = false }},
+				new UnionOfIntervals.Interval() {left = new UnionOfIntervals.Entry() {value = 5, isOpenInterval = false}, right = new UnionOfIntervals.Entry() {value = 7, isOpenInterval = true }},
+				new UnionOfIntervals.Interval() {left = new UnionOfIntervals.Entry() {value = 7, isOpenInterval = false}, right = new UnionOfIntervals.Entry() {value = 8, isOpenInterval = true }},
+				new UnionOfIntervals.Interval() {left = new UnionOfIntervals.Entry() {value = 8, isOpenInterval = false}, right = new UnionOfIntervals.Entry() {value = 11, isOpenInterval = true }},
+				new UnionOfIntervals.Interval() {left = new UnionOfIntervals.Entry() {value = 9, isOpenInterval = true}, right = new UnionOfIntervals.Entry() {value = 11, isOpenInterval = false }},
+				new UnionOfIntervals.Interval() {left = new UnionOfIntervals.Entry() {value = 12, isOpenInterval = false}, right = new UnionOfIntervals.Entry() {value = 14, isOpenInterval = false }},
+				new UnionOfIntervals.Interval() {left = new UnionOfIntervals.Entry() {value = 12, isOpenInterval = true}, right = new UnionOfIntervals.Entry() {value = 16, isOpenInterval = false }},
+				new UnionOfIntervals.Interval() {left = new UnionOfIntervals.Entry() {value = 13, isOpenInterval = true}, right = new UnionOfIntervals.Entry() {value = 15, isOpenInterval = true }},
+				new UnionOfIntervals.Interval() {left = new UnionOfIntervals.Entry() {value = 16, isOpenInterval = true}, right = new UnionOfIntervals.Entry() {value = 17, isOpenInterval = true }},
+			};
+
+			UnionOfIntervals.FindUnionOfIntervals(intervals).
+				ShouldBeEquivalentTo(new List<UnionOfIntervals.Interval>()
+				{
+					new UnionOfIntervals.Interval() {left = new UnionOfIntervals.Entry() {value = 0, isOpenInterval = true}, right = new UnionOfIntervals.Entry() {value = 4, isOpenInterval = false }},
+					new UnionOfIntervals.Interval() {left = new UnionOfIntervals.Entry() {value = 5, isOpenInterval = false}, right = new UnionOfIntervals.Entry() {value = 11, isOpenInterval = false }},
+					new UnionOfIntervals.Interval() {left = new UnionOfIntervals.Entry() {value = 12, isOpenInterval = false}, right = new UnionOfIntervals.Entry() {value = 17, isOpenInterval = true }}
+				});
+		}
+	}
+}
