78badd79b6fe8299cc12e97d364768a1fca3ac9e
Dipak Boyed <dipakboyed@hotmail.com>
2016-06-05 13:49:31 -0700

    Salary cap

---
+++ EPIProblems/Sorting/SalaryCap.cs
@@ -0,0 +1,34 @@
+﻿namespace EPI.Sorting
+{
+	/// <summary>
+	/// Design an algorithm for computing the salary cap given existing salaries and the target payroll budget.
+	/// Salaries less than equal to the cap remain unchanged while salaries greater than the cap are well capped.
+	/// </summary>
+	/// <example>
+	/// If existing salaries are: {90, 40, 20, 100, 30} and the payroll budget is 210, then the salary cap
+	/// </example>
+	public static class SalaryCap
+	{
+		public static double FindSalaryCap(double[] salaries, double budget)
+		{
+			// sort the salaries
+			QuickSort<double>.Sort(salaries);
+			double sumSoFar = 0;
+			double remainingSumCappedAtCurrentSalary = 0;
+			for (int i = 0; i < salaries.Length; i++)
+			{
+				remainingSumCappedAtCurrentSalary = salaries[i] * (salaries.Length - i);
+
+				if (sumSoFar + remainingSumCappedAtCurrentSalary >= budget)
+				{
+					// this the smallest salary where we are exceeding the cap
+					// cap is the average of the budget - sumSoFar
+					return (budget - sumSoFar) / (salaries.Length - i);
+				}
+				sumSoFar += salaries[i];
+			}
+
+			return -1; // no solution exists
+		}
+	}
+}
---
+++ EPIUnitTests/Sorting/SalaryCapUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using EPI.Sorting;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.Sorting
+{
+	[TestClass]
+	public class SalaryCapUnitTest
+	{
+		[TestMethod]
+		public void FindSalary()
+		{
+			SalaryCap.FindSalaryCap(new double[] { 20, 40, 100, 30, 90 }, 210).Should().Be(60);
+		}
+	}
+}
