2909064519eba7f10c20e3eebec2c91954f5c58d
Dipak Boyed <dipakboyed@hotmail.com>
2016-06-05 12:04:25 -0700

    Team photo day

---
+++ EPIProblems/Sorting/TeamPhotoDay.cs
@@ -0,0 +1,40 @@
+﻿namespace EPI.Sorting
+{
+	/// <summary>
+	/// Design an algorithm that takes as input two teams and the height of the players and checks if its
+	/// possible to place players to take a photo subject to the placement constraint.
+	/// The palcement constraint requires both teams to have the same number of players. All team members
+	/// must be in the same row. And all players in the back row must be taller than the player in front of them.
+	/// </summary>
+	public static class TeamPhotoDay
+	{
+		public static bool IsPhotoPlacementPossible(int[] teamAPlayerHeights, int[] teamBPlayerHeights)
+		{
+			// sort each team's player height
+			QuickSort<int>.Sort(teamAPlayerHeights);
+			QuickSort<int>.Sort(teamBPlayerHeights);
+
+			// let player at index 0 decide which team can be at front row
+			bool isTeamAAtFrontRow = teamAPlayerHeights[0] < teamBPlayerHeights[0];
+			for (int i = 0; i < teamAPlayerHeights.Length && i < teamBPlayerHeights.Length; i++)
+			{
+				if (isTeamAAtFrontRow)
+				{
+					if (teamAPlayerHeights[i] >= teamBPlayerHeights[i])
+					{
+						return false;
+					}
+				}
+				else // team B is front row
+				{
+					if (teamBPlayerHeights[i] >= teamAPlayerHeights[i])
+					{
+						return false;
+					}
+				}
+			}
+
+			return true;
+		}
+	}
+}
---
+++ EPIUnitTests/Sorting/TeamPhotoDayUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using EPI.Sorting;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.Sorting
+{
+	[TestClass]
+	public class TeamPhotoDayUnitTest
+	{
+		[TestMethod]
+		public void IsTeamPhotoPossible()
+		{
+			TeamPhotoDay.IsPhotoPlacementPossible(
+				new int[] { 5,1,3,8,9,1,4,8,6,5,6},
+				new int[] { 2,2,4,4,6,6,8,8,10,10,10}).Should().BeFalse();
+
+			TeamPhotoDay.IsPhotoPlacementPossible(
+				new int[] { 2, 1, 3, 11, 9, 10, 4, 8, 7, 5, 6 },
+				new int[] { 3, 2, 4, 5, 7, 6, 9, 8, 11, 12, 10 }).Should().BeTrue();
+		}
+	}
+}
