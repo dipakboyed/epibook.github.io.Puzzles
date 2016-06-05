bb29dcc6d1eae93e1f8a403cbfd71f816f85b943
Dipak Boyed <dipakboyed@hotmail.com>
2016-06-05 12:37:43 -0700

    linked list sort stable

---
+++ EPIProblems/Sorting/StableSortLinkedList.cs
@@ -0,0 +1,44 @@
+﻿using EPI.LinkedLists;
+
+namespace EPI.Sorting
+{
+	/// <summary>
+	/// Implement a function which sorts linked list efficiently.
+	/// It should be stable sort (i.e. relative position of equal elements is preserved)
+	/// </summary>
+	public static class StableSortLinkedList
+	{
+		public static SingleLinkedList<int> Sort(SingleLinkedList<int> list)
+		{
+			// Usually quick sort is the best for sorting arrays (avg. O(nlogn) runtime)
+			// but it isnt a stable sorting algorithm.
+			// Merge sort is stable but the default case does not do in-place sorting
+			// For linked lists we can do merge sort in-place
+
+			//recursion base case
+			if ((list == null) || (list.Head == null) || (list.Head.Next == null))
+			{
+				return list;
+			}
+
+			// use slow and fast pointers to find the mid-point of the linked list
+			SingleLinkedListNode<int> slow = list.Head;
+			SingleLinkedListNode<int> fast = list.Head;
+			SingleLinkedListNode<int> prevSlow = null;
+			while (fast != null && fast.Next != null)
+			{
+				prevSlow = slow;
+				slow = slow.Next;
+				fast = fast.Next.Next;
+			}
+
+			//terminate first first prior to slow
+			prevSlow.Next = null;
+
+			list = MergeTwoSortedLists.Merge(
+				Sort(new SingleLinkedList<int>() { Head = list.Head }),
+				Sort(new SingleLinkedList<int>() { Head = slow }));
+			return list;
+		}
+	}
+}
---
+++ EPIUnitTests/Sorting/StableSortLinkedListUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using EPI.LinkedLists;
+using EPI.Sorting;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.Sorting
+{
+	[TestClass]
+	public class StableSortLinkedListUnitTest
+	{
+		[TestMethod]
+		public void StableSortaLinkedList()
+		{
+			SingleLinkedList<int> list = new SingleLinkedList<int>()
+			{
+				Head = new SingleLinkedListNode<int>(5,
+					new SingleLinkedListNode<int>(2,
+						new SingleLinkedListNode<int>(3,
+							new SingleLinkedListNode<int>(4, null))))
+			};
+			StableSortLinkedList.Sort(list).ShouldBeEquivalentTo(
+				new SingleLinkedList<int>()
+				{
+					Head = new SingleLinkedListNode<int>(2,
+						new SingleLinkedListNode<int>(3,
+							new SingleLinkedListNode<int>(4,
+								new SingleLinkedListNode<int>(5, null))))
+				}
+				);
+		}
+	}
+}
