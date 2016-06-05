cc964677d4996ad438c7238d6ce55b3642d74178
Dipak Boyed <dipakboyed@hotmail.com>
2016-06-05 13:11:22 -0700

    smallest amount of change not possible to construct from given coins

---
+++ EPIProblems/Sorting/SmallestNonconstructibleChange.cs
@@ -0,0 +1,36 @@
+﻿namespace EPI.Sorting
+{
+	/// <summary>
+	/// Write a function which takes an array of positive integers and returns the smallest
+	/// number which is not the sum of a subset of elements in the array
+	/// </summary>
+	/// <example>
+	/// You have an array of coins {1,1,1,1,1,5,10,25}
+	/// The smallest amount of change that is not possible to construct from this array is 21
+	/// </example>
+	public static class SmallestNonconstructibleChange
+	{
+		public static int FindSmallestNonconstructibleChange(int[] array)
+		{
+			// sort the array
+			QuickSort<int>.Sort(array);
+
+			// Let S be the sum of all the elements in the array
+			// we definitely cannot create a change for S+1
+
+			// apply this same rule iteratively by comparing the next sorted
+			// element against the sum of all elements so far (Si + 1)
+			// If next sorted element An is > Sum(A[0..n-1]), then we found our answer
+			int sumOfChange = 0;
+			foreach (int item in array)
+			{
+				if (item > sumOfChange + 1)
+				{
+					break;
+				}
+				sumOfChange += item;
+			}
+			return sumOfChange + 1;
+		}
+	}
+}
---
+++ EPIUnitTests/Sorting/SmallestNonconstructibleChangeUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using EPI.Sorting;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.Sorting
+{
+	[TestClass]
+	public class SmallestNonconstructibleChangeUnitTest
+	{
+		[TestMethod]
+		public void FindSmallestChangeNotPossibleToConstruct()
+		{
+			SmallestNonconstructibleChange.FindSmallestNonconstructibleChange(new[] { 1, 1, 1, 1, 1, 5, 10, 25 }).Should().Be(21);
+		}
+	}
+}
