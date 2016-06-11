e158279de04fce3fff890d2f91a1bcbd24c3f3dc
Dipak Boyed <dipakboyed@hotmail.com>
2016-06-11 12:41:53 -0700

    Find leaf node with given path weight

---
+++ EPIProblems/BinaryTree/FindPathSum.cs
@@ -0,0 +1,31 @@
+﻿namespace EPI.BinaryTree
+{
+	/// <summary>
+	/// Write a function that takes an integer and a binary tree with integer node values and checks
+	/// if there exists a leaf node whose path weight equals the given integer.
+	/// Path weight of a node is the sum of the integers on the unique path from the root to that node
+	/// </summary>
+	public static class FindPathSum
+	{
+		public static bool HasPathWeight(int weight, BinaryTreeNode<int> root)
+		{
+			return PathWeightHelper(weight, root, 0);
+		}
+
+		private static bool PathWeightHelper(int weight, BinaryTreeNode<int> node, int pathSumSoFar)
+		{
+			if (node == null)
+			{
+				return false;
+			}
+
+			pathSumSoFar += node.Value;
+			if (node.Left == null && node.Right == null) //leaf nodes
+			{
+				return pathSumSoFar == weight;
+			}
+
+			return PathWeightHelper(weight, node.Left, pathSumSoFar) || PathWeightHelper(weight, node.Right, pathSumSoFar);
+		}
+	}
+}
---
+++ EPIUnitTests/BinaryTree/FindPathSumUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using EPI.BinaryTree;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.BinaryTree
+{
+	[TestClass]
+	public class FindPathSumUnitTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			BinaryTree<int> tree = new BinaryTree<int>()
+			{
+				Root = new BinaryTreeNode<int>(314)
+				{
+					Left = new BinaryTreeNode<int>(6)
+					{
+						Left = new BinaryTreeNode<int>(271)
+						{
+							Right = new BinaryTreeNode<int>(1)
+						},
+						Right = new BinaryTreeNode<int>(561)
+						{
+							Left = new BinaryTreeNode<int>(3)
+						}
+					},
+					Right = new BinaryTreeNode<int>(6)
+					{
+						Left = new BinaryTreeNode<int>(2)
+						{
+							Right = new BinaryTreeNode<int>(1)
+							{
+								Left = new BinaryTreeNode<int>(257)
+							}
+						}
+					},
+				}
+			};
+			FindPathSum.HasPathWeight(592, tree.Root).Should().BeTrue();
+			FindPathSum.HasPathWeight(591, tree.Root).Should().BeFalse();
+			FindPathSum.HasPathWeight(581, tree.Root).Should().BeFalse();
+		}
+	}
+}
