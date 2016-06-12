9a12cf04d757a96277858e54d49e78145a5fc425
Dipak Boyed <dipakboyed@hotmail.com>
2016-06-12 12:25:10 -0700

    Find successor of inorder traversal

---
+++ EPIProblems/BinaryTree/FindSuccessorInorderTraversal.cs
@@ -0,0 +1,50 @@
+﻿namespace EPI.BinaryTree
+{
+	/// <summary>
+	/// Design an algorithm to compute the successor of a node in a binary tree.
+	/// Assume that each node stores it's parent.
+	/// The successor is the node that appears immediately after the given node in an inorder traversal.
+	/// </summary>
+	public static class FindSuccessorInorderTraversal<T>
+	{
+		public static BinaryTreeNode<T> FindSuccessor(BinaryTreeNode<T> node)
+		{
+			if (node != null)
+			{
+				if (node.Right != null) //right subtree exists
+				{
+					// in-order traversal means successor will be in the right subtree
+					BinaryTreeNode<T> current = node.Right;
+
+					// it will be the leftmost node in the right subtree
+					while (current.Left != null)
+					{
+						current = current.Left;
+					}
+					return current;
+				}
+
+				// right subtree doesnt exist
+				if (node.Parent != null)
+				{
+					// walk up to parents to find the successor
+					BinaryTreeNode<T> next = node.Parent;
+
+					while (next != null && node.Equals(next.Right))
+					{
+						// current node is the right child of the next node
+						// keep walking back up the ancestors
+						node = next;
+						next = next.Parent;
+					}
+
+					// current node is the left child of the next node
+					// return next node
+					return next;
+				}
+			}
+
+			return null;
+		}
+	}
+}
---
+++ EPIUnitTests/BinaryTree/FindSuccessorInorderTraversalUnitTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using EPI.BinaryTree;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.BinaryTree
+{
+	[TestClass]
+	public class FindSuccessorInorderTraversalUnitTest
+	{
+		[TestMethod]
+		public void FindInorderSuccessor()
+		{
+			BinaryTree<char> tree = new BinaryTree<char>()
+			{
+				Root = new BinaryTreeNode<char>('A')
+				{
+					Left = new BinaryTreeNode<char>('B')
+					{
+						Left = new BinaryTreeNode<char>('C')
+						{
+							Right = new BinaryTreeNode<char>('D')
+						},
+						Right = new BinaryTreeNode<char>('E')
+						{
+							Left = new BinaryTreeNode<char>('F')
+						}
+					},
+					Right = new BinaryTreeNode<char>('G')
+					{
+						Left = new BinaryTreeNode<char>('H')
+						{
+							Right = new BinaryTreeNode<char>('I')
+							{
+								Left = new BinaryTreeNode<char>('J')
+							}
+						}
+					}
+				}
+			};
+
+			FindSuccessorInorderTraversal<char>.FindSuccessor(tree.Root).Value.Should().Be('H');
+			FindSuccessorInorderTraversal<char>.FindSuccessor(tree.Root.Left).Value.Should().Be('F');
+			FindSuccessorInorderTraversal<char>.FindSuccessor(tree.Root.Right).Should().BeNull();
+
+			FindSuccessorInorderTraversal<char>.FindSuccessor(tree.Root.Left.Left).Value.Should().Be('D');
+			FindSuccessorInorderTraversal<char>.FindSuccessor(tree.Root.Left.Left.Right).Value.Should().Be('B');
+			FindSuccessorInorderTraversal<char>.FindSuccessor(tree.Root.Left.Right).Value.Should().Be('A');
+			FindSuccessorInorderTraversal<char>.FindSuccessor(tree.Root.Left.Right.Left).Value.Should().Be('E');
+		}
+	}
+}
