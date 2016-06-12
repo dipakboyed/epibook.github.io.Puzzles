faed5487e5ba8dc2b6203ad8d1896d3a881779f9
Dipak Boyed <dipakboyed@hotmail.com>
2016-06-12 11:11:23 -0700

    Compute exterior of binary tree

---
+++ EPIProblems/BinaryTree/BinaryTreeExterior.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace EPI.BinaryTree
+{
+	/// <summary>
+	/// Write a function that computes the exterior of a binary tree.
+	/// The exterior is the following sequence of nodes:
+	///   the nodes from the root to the leftmost leaf,
+	///   followed by the leaves in left-to-right order,
+	///   followed by the nodes from the rightmost leaf to the root
+	/// </summary>
+	public static class BinaryTreeExterior<T>
+	{
+		public static List<T> PrintExterior(BinaryTreeNode<T> root)
+		{
+			List<T> result = new List<T>();
+			if (root != null)
+			{
+				result.Add(root.Value);
+			}
+			PrintLeftSubtreeExteriorAndLeaves(root.Left, result, true);
+			PrintRightSubtreeLeavesAndExterior(root.Right, result, true);
+			return result;
+		}
+
+		private static void PrintLeftSubtreeExteriorAndLeaves(BinaryTreeNode<T> node, List<T> result, bool isNonLeafExterior)
+		{
+			if (node != null)
+			{
+				if (isNonLeafExterior || IsLeafNode(node))
+				{
+					result.Add(node.Value);
+				}
+				PrintLeftSubtreeExteriorAndLeaves(node.Left,result, isNonLeafExterior);
+				PrintLeftSubtreeExteriorAndLeaves(node.Right, result, isNonLeafExterior && node.Left == null);
+			}
+		}
+
+		private static void PrintRightSubtreeLeavesAndExterior(BinaryTreeNode<T> node, List<T> result, bool isNonLeafExterior)
+		{
+			if (node != null)
+			{
+				PrintRightSubtreeLeavesAndExterior(node.Left, result, isNonLeafExterior && node.Right == null);
+				PrintRightSubtreeLeavesAndExterior(node.Right, result, isNonLeafExterior);
+				if (isNonLeafExterior || IsLeafNode(node))
+				{
+					result.Add(node.Value);
+				}
+			}
+		}
+
+		private static bool IsLeafNode(BinaryTreeNode<T> node)
+		{
+			return (node != null && node.Left == null && node.Right == null);
+		}
+	}
+}
---
+++ EPIUnitTests/BinaryTree/BinaryTreeExteriorUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using EPI.BinaryTree;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.BinaryTree
+{
+	[TestClass]
+	public class BinaryTreeExteriorUnitTest
+	{
+		[TestMethod]
+		public void PrintExteriorBinaryTree()
+		{
+			BinaryTree<char> tree = new BinaryTree<char>()
+			{
+				Root = new BinaryTreeNode<char>('A')
+				{
+					Left = new BinaryTreeNode<char>('B')
+					{
+						Left = new BinaryTreeNode<char>('C')
+						{
+							Right = new BinaryTreeNode<char>('D')
+						},
+						Right = new BinaryTreeNode<char>('E')
+						{
+							Left = new BinaryTreeNode<char>('F')
+						}
+					},
+					Right = new BinaryTreeNode<char>('G')
+					{
+						Left = new BinaryTreeNode<char>('H')
+						{
+							Right = new BinaryTreeNode<char>('I')
+							{
+								Left = new BinaryTreeNode<char>('J')
+							}
+						}
+					}
+				}
+			};
+
+			BinaryTreeExterior<char>.PrintExterior(tree.Root).ShouldBeEquivalentTo(
+				new List<char>() { 'A', 'B', 'C', 'D', 'F', 'J', 'I', 'H' ,'G'});
+		}
+	}
+}
