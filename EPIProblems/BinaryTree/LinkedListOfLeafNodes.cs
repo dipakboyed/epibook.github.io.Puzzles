30d38f267cba86f70de0049cf5a6087be6e1a568
Dipak Boyed <dipakboyed@hotmail.com>
2016-06-12 15:27:31 -0700

    list leaf nodes

---
+++ EPIProblems/BinaryTree/LinkedListOfLeafNodes.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace EPI.BinaryTree
+{
+	/// <summary>
+	/// Give a binary tree, compute a linked list from the leaves of the binary tree
+	/// where the leaves appear in left-to-right order
+	/// </summary>
+	public static class LinkedListOfLeafNodes<T>
+	{
+		public static LinkedList<T> BuildListOfLeafNodes(BinaryTreeNode<T> root)
+		{
+			LinkedList<T> result = new LinkedList<T>();
+			BuildLeafNodesListHelper(root, result);
+			return result;
+		}
+
+		private static void BuildLeafNodesListHelper(BinaryTreeNode<T> node, LinkedList<T> result)
+		{
+			if (node == null)
+			{
+				return;
+			}
+
+			// recursively do in-order traversal
+			if (node.Left == null && node.Right == null)
+			{
+				// leaf node
+				result.AddLast(node.Value);
+			}
+
+			BuildLeafNodesListHelper(node.Left, result);
+			BuildLeafNodesListHelper(node.Right, result);
+		}
+	}
+}
---
+++ EPIUnitTests/BinaryTree/LinkedListOfLeafNodesUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using EPI.BinaryTree;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.BinaryTree
+{
+	[TestClass]
+	public class LinkedListOfLeafNodesUnitTest
+	{
+		[TestMethod]
+		public void BuildLeafNodesList()
+		{
+			BinaryTree<char> tree = new BinaryTree<char>()
+			{
+				Root = new BinaryTreeNode<char>('A')
+				{
+					Left = new BinaryTreeNode<char>('B')
+					{
+						Left = new BinaryTreeNode<char>('C')
+						{
+							Right = new BinaryTreeNode<char>('D')
+						},
+						Right = new BinaryTreeNode<char>('E')
+						{
+							Left = new BinaryTreeNode<char>('F')
+						}
+					},
+					Right = new BinaryTreeNode<char>('G')
+					{
+						Left = new BinaryTreeNode<char>('H')
+						{
+							Right = new BinaryTreeNode<char>('I')
+							{
+								Left = new BinaryTreeNode<char>('J')
+							}
+						}
+					}
+				}
+			};
+			LinkedListOfLeafNodes<char>.BuildListOfLeafNodes(tree.Root).ShouldBeEquivalentTo(
+				new List<char>() { 'D', 'F', 'J'});
+
+		}
+	}
+}
