365740cea24e1b96d4a09a02b2b735621751b481
Dipak Boyed <dipakboyed@hotmail.com>
2016-06-16 11:57:47 -0700

    IsBinarySearchTree check

---
+++ EPIProblems/BinaryTree/BinarySearchTree/IsBinarySearchTree.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace EPI.BinaryTree.BinarySearchTree
+{
+	public static class IsBinarySearchTree
+	{
+		public static bool CheckBSTPropertyRecursive(BinaryTreeNode<int> node)
+		{
+			if (node == null)
+			{
+				return true;
+			}
+			else if ((node.Left != null && node.Value < node.Left.Value) || (node.Right != null && node.Value > node.Right.Value))
+			{
+				return false;
+			}
+
+			return CheckBSTPropertyRecursive(node.Left) && CheckBSTPropertyRecursive(node.Right);
+		}
+
+		public static bool CheckBSTProperty(BinaryTreeNode<int> root)
+		{
+			Queue<BinaryTreeNode<int>> nodes = new Queue<BinaryTreeNode<int>>();
+			if (root != null)
+			{
+				nodes.Enqueue(root);
+			}
+
+			while (nodes.Count > 0)
+			{
+				var currentNode = nodes.Dequeue();
+				if (currentNode.Left != null)
+				{
+					if (currentNode.Value < currentNode.Left.Value)
+					{
+						return false;
+					}
+					nodes.Enqueue(currentNode.Left);
+				}
+				if (currentNode.Right != null)
+				{
+					if (currentNode.Value > currentNode.Right.Value)
+					{
+						return false;
+					}
+					nodes.Enqueue(currentNode.Right);
+				}
+			}
+
+			return true;
+		}
+	}
+}
---
+++ EPIUnitTests/BinaryTree/BinarySearchTree/IsBinarySearchTreeUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using EPI.BinaryTree;
+using EPI.BinaryTree.BinarySearchTree;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.BinaryTree.BinarySearchTree
+{
+	[TestClass]
+	public class IsBinarySearchTreeUnitTest
+	{
+		[TestMethod]
+		public void CheckIfBinarySearchTree()
+		{
+			BinaryTree<int> bst = new BinaryTree<int>()
+			{
+				Root = new BinaryTreeNode<int>(50)
+				{
+					Left = new BinaryTreeNode<int>(40)
+					{
+						Left = new BinaryTreeNode<int>(30)
+						{
+							Right = new BinaryTreeNode<int>(35)
+						},
+						Right = new BinaryTreeNode<int>(45)
+						{
+							Left = new BinaryTreeNode<int>(42)
+						}
+					},
+					Right = new BinaryTreeNode<int>(60)
+					{
+						Left = new BinaryTreeNode<int>(20)
+						{
+							Right = new BinaryTreeNode<int>(30)
+							{
+								Left = new BinaryTreeNode<int>(25)
+							}
+						}
+					},
+				}
+			};
+
+			IsBinarySearchTree.CheckBSTPropertyRecursive(bst.Root).Should().BeTrue();
+			IsBinarySearchTree.CheckBSTProperty(bst.Root).Should().BeTrue();
+		}
+	}
+}
