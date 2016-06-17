e4f0ddcc9f6f32f7487b928482c72f3c5581dd32
Dipak Boyed <diboyed@microsoft.com>
2016-06-16 19:56:49 -0700

    x/y without using divide, modulo operator

---
+++ EPIProblems/PrimitiveTypes/Divide.cs
@@ -0,0 +1,45 @@
+﻿namespace EPI.PrimitiveTypes
+{
+	/// <summary>
+	/// Given two positive integers (x and y), compute their quotient (x/y) using
+	/// only the addition, subtraction and shifting operators
+	/// </summary>
+	public static class Divide
+	{
+		public static int ComputeXDivideByY(int x, int y)
+		{
+			int quotient = 0;
+			if (x > 0 && y > 0)
+			{
+				while (x >= y)
+				{
+					x -= y;
+					quotient++;
+				}
+			}
+			return quotient;
+		}
+
+		public static int ComputeXDivideByYFaster(int x, int y)
+		{
+			int quotient = 0;
+			if (x > 0 && y > 0)
+			{
+				while (x >= y)
+				{
+					int power = 1;
+
+					while ((y << power) >= (y << (power - 1)) /* no overflow */ && 
+							( y << power) <= x)
+					{
+						power++;
+					}
+
+					quotient += 1 << (power - 1);
+					x -= y << (power - 1);
+				}
+			}
+			return quotient;
+		}
+	}
+}
---
+++ EPIUnitTests/PrimitiveTypes/DivideUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using EPI.PrimitiveTypes;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.PrimitiveTypes
+{
+	[TestClass]
+	public class DivideUnitTest
+	{
+		[TestMethod]
+		public void DivideXbyY()
+		{
+			Divide.ComputeXDivideByY(1, 1).Should().Be(1);
+			Divide.ComputeXDivideByY(2, 1).Should().Be(2);
+			Divide.ComputeXDivideByY(5, 2).Should().Be(2);
+			Divide.ComputeXDivideByY(22, 7).Should().Be(3);
+
+			Divide.ComputeXDivideByYFaster(1, 1).Should().Be(1);
+			Divide.ComputeXDivideByYFaster(2, 1).Should().Be(2);
+			Divide.ComputeXDivideByYFaster(5, 2).Should().Be(2);
+			Divide.ComputeXDivideByYFaster(22, 7).Should().Be(3);
+		}
+	}
+}
+
