f26c8e1c171cd1f3f3777b5f901a5aad5c78535f
Dipak Boyed <dipakboyed@hotmail.com>
2016-06-17 15:30:39 -0700

    x power y

---
+++ EPIProblems/PrimitiveTypes/Power.cs
@@ -0,0 +1,28 @@
+﻿namespace EPI.PrimitiveTypes
+{
+	/// <summary>
+	/// Write a function that takes a double x and an integer y and returns x^y.
+	/// Assume addition, multiplication and division take constant time.
+	/// Ignore overflow/underflow exceptions
+	/// </summary>
+	public static class Power
+	{
+		public static double XPowerY(double x, int y)
+		{
+			double result = 1;
+			while (y > 0)
+			{
+				// iterate through each bit of y
+
+				if ((y & 1) == 1) // LSB ends with 1
+				{
+					result *= x; // current x value counts toawrds the final result
+				}
+				x *= x; // double the x value with each iteration
+				y >>= 1; // shift to next bit
+			}
+
+			return result;
+		}
+	}
+}
---
+++ EPIUnitTests/PrimitiveTypes/PowerUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using EPI.PrimitiveTypes;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.PrimitiveTypes
+{
+	[TestClass]
+	public class PowerUnitTest
+	{
+		[TestMethod]
+		public void PowerXbyY()
+		{
+			Power.XPowerY(1, 1).Should().Be(1);
+			Power.XPowerY(2, 0).Should().Be(1);
+			Power.XPowerY(5, 2).Should().Be(25);
+			Power.XPowerY(22, 7).Should().Be(2494357888);
+		}
+	}
+}
+
