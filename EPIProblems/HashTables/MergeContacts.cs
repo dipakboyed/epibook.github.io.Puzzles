bdc61fed5245e5e903f4462597f05f5d37032db5
Dipak Boyed <dipakboyed@hotmail.com>
2016-06-21 11:51:24 -0700

    merge contacts

---
+++ EPIProblems/HashTables/MergeContacts.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace EPI.HashTables
+{
+	/// <summary>
+	/// You are given a list of contacts (containing name and one or more emails) from various sources
+	/// and you need to join all contacts with the same emails and return distinct sets of names
+	/// </summary>
+	/// <example>
+	/// {Angela - a @fb.com, b @fb.com}
+	/// {Angie - b @fb.com, c @fb.com}
+	/// {A. L. - c @fb.com}
+	/// {Bob   - z @fb.com}
+	/// returns:
+	/// [Angela, Angie, A.L.], [Bob]
+	/// </example>
+	public static class MergeContacts
+	{
+
+		private class MergedContacts
+		{
+			public HashSet<string> Names = new HashSet<string>();
+			public HashSet<string> Emails = new HashSet<string>();
+		};
+
+		public class Contact
+		{
+			public string Name;
+			public List<string> Emails = new List<string>();
+		};
+
+		public static List<List<string>> MergeContactNamesWithSameEmails(List<Contact> contacts)
+		{
+			List<MergedContacts> distinctContacts = new List<MergedContacts>();
+			Dictionary<string, int> emailToListIndex = new Dictionary<string, int>();
+
+			foreach (var contact in contacts)
+			{
+				int emailExistsIndex = -1;
+				foreach (string email in contact.Emails)
+				{
+					if (emailToListIndex.ContainsKey(email))
+					{
+						emailExistsIndex = emailToListIndex[email];
+						break;
+					}
+				}
+
+				if (emailExistsIndex >= 0)
+				{
+					if (!distinctContacts[emailExistsIndex].Names.Contains(contact.Name))
+					{
+						distinctContacts[emailExistsIndex].Names.Add(contact.Name);
+					}
+					foreach (var email in contact.Emails)
+					{
+						if (!distinctContacts[emailExistsIndex].Emails.Contains(email))
+						{
+							distinctContacts[emailExistsIndex].Emails.Add(email);
+							emailToListIndex.Add(email, emailExistsIndex);
+						}
+					}
+				}
+				else
+				{
+					MergedContacts c = new MergedContacts();
+					c.Names.Add(contact.Name);
+					foreach (var email in contact.Emails)
+					{
+						c.Emails.Add(email);
+						emailToListIndex.Add(email, distinctContacts.Count);
+					}
+					distinctContacts.Add(c);
+				}
+			}
+
+			List<List<string>> result = new List<List<string>>();
+			foreach (var distinctContact in distinctContacts)
+			{
+				result.Add(distinctContact.Names.ToList());
+			}
+			return result;
+		}
+	}
+}
---
+++ EPIUnitTests/HashTables/MergeContactsUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using EPI.HashTables;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.HashTables
+{
+	[TestClass]
+	public class MergeContactsUnitTest
+	{
+		[TestMethod]
+		public void MergeContactsWithSameEmails()
+		{
+			List<MergeContacts.Contact> names = new List<MergeContacts.Contact>()
+			{
+				new MergeContacts.Contact() {Name = "Abby", Emails = new List<string>() { "a@a.com", "b@a.com"} },
+				new MergeContacts.Contact() {Name = "Abbie", Emails = new List<string>() { "c@a.com", "b@a.com"} },
+				new MergeContacts.Contact() {Name = "A.B.", Emails = new List<string>() { "c@a.com"} },
+				new MergeContacts.Contact() {Name = "Bob", Emails = new List<string>() { "z@a.com"} }
+			};
+
+			MergeContacts.MergeContactNamesWithSameEmails(names).ShouldBeEquivalentTo(new List<List<string>>()
+			{
+				new List<string>() { "Abby", "Abbie", "A.B." },
+				new List<string>() {"Bob" }
+			}
+			);
+		}
+	}
+}
