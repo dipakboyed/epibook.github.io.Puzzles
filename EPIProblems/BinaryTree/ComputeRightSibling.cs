6a35a4831a11a27a8ff81f3a23336ad72dac1220
Dipak Boyed <dipakboyed@hotmail.com>
2016-06-25 11:46:27 -0700

    right sibling in a perfect binary tree

---
+++ EPIProblems/BinaryTree/ComputeRightSibling.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPI.BinaryTree
+{
+	public class BinaryTreeNodeWithNext<U>
+	{
+		public BinaryTreeNodeWithNext<U> Next;
+		public BinaryTreeNodeWithNext<U> Left;
+		public BinaryTreeNodeWithNext<U> Right;
+		public U Val;
+		public BinaryTreeNodeWithNext(U value)
+		{
+			Val = value;
+			Left = null;
+			Right = null;
+			Next = null;
+		}
+	}
+
+	/// <summary>
+	/// Write a function that takes a perfect binary tree and sets each node's next field to the
+	/// node on its right, if one exists
+	/// </summary>
+	public static class ComputeRightSibling<T>
+	{
+		public static void SetRightSibling(BinaryTreeNodeWithNext<T> root)
+		{
+			while (root != null)
+			{
+				SetNextFieldInChildren(root);
+				root = root.Left;
+			}
+		}
+
+		private static void SetNextFieldInChildren(BinaryTreeNodeWithNext<T> node)
+		{
+			while (node != null && node.Left != null)
+			{
+				node.Left.Next = node.Right;
+				if (node.Next != null)
+				{
+					node.Right.Next = node.Next.Left;
+				}
+				node = node.Next;
+			}
+		}
+	}
+}
---
+++ EPIUnitTests/BinaryTree/ComputeRightSiblingUnitTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using EPI.BinaryTree;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.BinaryTree
+{
+	[TestClass]
+	public class ComputeRightSiblingUnitTest
+	{
+		[TestMethod]
+		public void FindRightSibling()
+		{
+			BinaryTreeNodeWithNext<char> Root = new BinaryTreeNodeWithNext<char>('A')
+			{
+				Left = new BinaryTreeNodeWithNext<char>('B')
+				{
+					Left = new BinaryTreeNodeWithNext<char>('C')
+					{
+						Left = new BinaryTreeNodeWithNext<char>('D'),
+						Right = new BinaryTreeNodeWithNext<char>('E')
+					},
+					Right = new BinaryTreeNodeWithNext<char>('F')
+					{
+						Left = new BinaryTreeNodeWithNext<char>('G'),
+						Right = new BinaryTreeNodeWithNext<char>('H')
+					}
+				},
+				Right = new BinaryTreeNodeWithNext<char>('I')
+				{
+					Left = new BinaryTreeNodeWithNext<char>('J')
+					{
+						Left = new BinaryTreeNodeWithNext<char>('K'),
+						Right = new BinaryTreeNodeWithNext<char>('L')
+					},
+					Right = new BinaryTreeNodeWithNext<char>('M')
+					{
+						Left = new BinaryTreeNodeWithNext<char>('N'),
+						Right = new BinaryTreeNodeWithNext<char>('O')
+					}
+				}
+			};
+
+			ComputeRightSibling<char>.SetRightSibling(Root);
+			Root.Next.Should().BeNull();
+			Root.Left.Next.Should().Be(Root.Right);
+			Root.Left.Left.Next.Should().Be(Root.Left.Right);
+
+		}
+	}
+}
