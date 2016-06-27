947dfffc6046bed6ff34d7c5eb7d6d510d3d6de4
Dipak Boyed <dipakboyed@hotmail.com>
2016-06-27 00:01:53 -0700

    locking in a binary tree

---
+++ EPIProblems/BinaryTree/BinaryTreeLocking.cs
@@ -0,0 +1,114 @@
+﻿namespace EPI.BinaryTree
+{
+	/// <summary>
+	/// Design an API for setting the state of nodes in a binary tree to lock or unlock.
+	/// A node cannot be set to lock if any of its descendants or ancestors are in lock.
+	/// Changing a node's state to lock does not change the state of any other node.
+	/// Assume that each node has a Parent field.`
+	/// </summary>
+	public static class BinaryTreeLocking<T>
+	{
+		public static bool IsNodeLocked(BinaryTreeNodeWithLock<T> node)
+		{
+			return node.isLocked;
+		}
+
+		public static bool LockNode(BinaryTreeNodeWithLock<T> node)
+		{
+			if (node.noOfLockedDescendants > 0 || node.isLocked)
+			{
+				return false;
+			}
+			var current = node.Parent;
+			while (current != null)
+			{
+				if (current.isLocked)
+				{
+					return false;
+				}
+				current = current.Parent;
+			}
+
+			node.isLocked = true;
+			while (node.Parent != null)
+			{
+				node.Parent.noOfLockedDescendants++;
+				node = node.Parent;
+			}
+			return true;
+		}
+
+		public static void UnlockNode(BinaryTreeNodeWithLock<T> node)
+		{
+			if (IsNodeLocked(node))
+			{
+				node.isLocked = false;
+				while (node.Parent != null)
+				{
+					node.Parent.noOfLockedDescendants--;
+					node = node.Parent;
+				}
+			}
+		}
+	}
+
+	public class BinaryTreeNodeWithLock<U>
+	{
+		public int noOfLockedDescendants;
+		public bool isLocked;
+		private U value;
+		private BinaryTreeNodeWithLock<U> left;
+		private BinaryTreeNodeWithLock<U> right;
+		public BinaryTreeNodeWithLock<U> Parent;
+
+		public BinaryTreeNodeWithLock(U val)
+		{
+			noOfLockedDescendants = 0;
+			isLocked = false;
+			value = val;
+			left = null;
+			right = null;
+			Parent = null;
+		}
+
+		public U Value
+		{
+			get
+			{
+				return value;
+			}
+		}
+
+		public BinaryTreeNodeWithLock<U> Left
+		{
+			get
+			{
+				return left;
+			}
+			set
+			{
+				left = value;
+				if (left != null)
+				{
+					left.Parent = this;
+				}
+			}
+		}
+
+		public BinaryTreeNodeWithLock<U> Right
+		{
+			get
+			{
+				return right;
+			}
+			set
+			{
+				right = value;
+				if (right != null)
+				{
+					right.Parent = this;
+				}
+			}
+		}
+	}
+}
---
+++ EPIUnitTests/BinaryTree/BinartyTreeLockingUnitTest.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using EPI.BinaryTree;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.BinaryTree
+{
+	[TestClass]
+	public class BinaryTreeLockingUnitTest
+	{
+		[TestMethod]
+		public void LockBinaryTree()
+		{
+			BinaryTreeNodeWithLock<char> Root = new BinaryTreeNodeWithLock<char>('A')
+			{
+				Left = new BinaryTreeNodeWithLock<char>('B')
+				{
+					Left = new BinaryTreeNodeWithLock<char>('C')
+					{
+						Left = new BinaryTreeNodeWithLock<char>('D'),
+						Right = new BinaryTreeNodeWithLock<char>('E')
+					},
+					Right = new BinaryTreeNodeWithLock<char>('F')
+					{
+						Left = new BinaryTreeNodeWithLock<char>('G'),
+						Right = new BinaryTreeNodeWithLock<char>('H')
+					}
+				},
+				Right = new BinaryTreeNodeWithLock<char>('I')
+				{
+					Left = new BinaryTreeNodeWithLock<char>('J')
+					{
+						Left = new BinaryTreeNodeWithLock<char>('K'),
+						Right = new BinaryTreeNodeWithLock<char>('L')
+					},
+					Right = new BinaryTreeNodeWithLock<char>('M')
+					{
+						Left = new BinaryTreeNodeWithLock<char>('N'),
+						Right = new BinaryTreeNodeWithLock<char>('O')
+					}
+				}
+			};
+
+			BinaryTreeLocking<char>.IsNodeLocked(Root).Should().BeFalse();
+			BinaryTreeLocking<char>.IsNodeLocked(Root.Left).Should().BeFalse();
+			BinaryTreeLocking<char>.IsNodeLocked(Root.Right.Right.Right).Should().BeFalse();
+
+			BinaryTreeLocking<char>.LockNode(Root.Left /* B */).Should().BeTrue();
+			BinaryTreeLocking<char>.LockNode(Root.Right.Right.Right /* O */).Should().BeTrue();
+
+			BinaryTreeLocking<char>.IsNodeLocked(Root).Should().BeFalse();
+			BinaryTreeLocking<char>.IsNodeLocked(Root.Left).Should().BeTrue();
+			BinaryTreeLocking<char>.IsNodeLocked(Root.Right.Right.Right).Should().BeTrue();
+
+			BinaryTreeLocking<char>.LockNode(Root).Should().BeFalse();
+			BinaryTreeLocking<char>.LockNode(Root.Left.Left).Should().BeFalse();
+
+			BinaryTreeLocking<char>.UnlockNode(Root.Left);
+
+			BinaryTreeLocking<char>.LockNode(Root).Should().BeFalse();
+			BinaryTreeLocking<char>.LockNode(Root.Left.Left).Should().BeTrue();
+		}
+	}
+}
