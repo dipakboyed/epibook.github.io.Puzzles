2c9af47045c49a6cda53ed5fa8b3d2adc8928a9f
Dipak Boyed <dipakboyed@hotmail.com>
2016-06-27 15:55:56 -0700

    towers of hanoi

---
+++ EPIProblems/Recursion/TowersOfHanoi.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace EPI.Recursion
+{
+	/// <summary>
+	/// You are given n rings. The i-th ring has diameter i. The rings are sorted in a peg P1
+	/// with the largest ring at the bottom. Transfer these rings to peg P3 which is initially empty.
+	/// You have an intermediate peg P2 which is also empty. The only operation allowed is to take the
+	/// top most ring from a peg and placing it on top of another peg. You can never place a bigger ring
+	/// above a smaller ring.
+	/// </summary>
+	public static class TowersOfHanoi
+	{
+		internal static List<Stack> pegs;
+
+		public static void TransferPegs(int n)
+		{
+			pegs = new List<Stack>()
+			{
+				new Stack(),
+				new Stack(),
+				new Stack()
+			};
+			for (int i=n; i > 0; i--)
+			{
+				pegs[0].Push(i);
+			}
+
+			MovePegs(n, 0, 2, 1);
+		}
+
+		private static void MovePegs(int n, int from, int to, int intermediate)
+		{
+			if (n > 0)
+			{
+				MovePegs(n - 1, from, intermediate, to);
+				pegs[to].Push(pegs[from].Pop());
+				MovePegs(n - 1, intermediate, to, from);
+			}
+		}
+	}
+}
---
+++ EPIUnitTests/Recursion/TowersOfHanoiUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using EPI.Recursion;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.Recursion
+{
+	[TestClass]
+	public class TowersOfHanoiUnitTest
+	{
+		[TestMethod]
+		public void TowersOfHanoiSorting()
+		{
+			TowersOfHanoi.TransferPegs(4);
+			TowersOfHanoi.pegs[0].Count.Should().Be(0);
+			TowersOfHanoi.pegs[2].Count.Should().Be(4);
+			for (int i= 1; i <= 4; i++)
+			{
+				TowersOfHanoi.pegs[2].Pop().Should().Be(i);
+			}
+		}
+	}
+}
