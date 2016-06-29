2b7ce69fbb3f6be49e6b3a28b0eb90a807a0fd9f
Dipak Boyed <dipakboyed@hotmail.com>
2016-06-28 23:34:15 -0700

    regex matching via recursion

---
+++ EPIProblems/Recursion/RegexMatching.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPI.Recursion
+{
+	/// <summary>
+	/// Design an algorithm that takes a string s and a string (assumed to be a well-formed
+	/// ESRE {Extended Simple Regular Expression}) and checks if r matches s.
+	/// </summary>
+	/// <remarks>
+	/// ESRE can contain alphanumeric character, dot character, * character, ^ and $ characters.
+	/// </remarks>
+	public static class RegexMatching
+	{
+		public static bool IsMatch(string s, string regex)
+		{
+			// regex starts with ^
+			if (regex.StartsWith("^"))
+			{
+				return IsMatchSubstring(s, 0, regex, 1);
+			}
+
+			for (int i = 0; i < s.Length; i++)
+			{
+				if (IsMatchSubstring(s, i, regex, 0))
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		private static bool IsMatchSubstring(string s, int sIndex, string regex, int regexIndex)
+		{
+			// base case
+			// we've reached the end of the regex string and not failed so far, success
+			if (regexIndex == regex.Length)
+			{
+				return true;
+			}
+
+			// regex endswith $
+			// success if we've also reached the end of the string, false otherwise
+			if (regexIndex == regex.Length -1 && regex[regexIndex] == '$')
+			{
+				return sIndex == s.Length;
+			}
+
+			// regex at current index begins with (char)*
+			if (regexIndex + 2 <= regex.Length && regex[regexIndex + 1] == '*')
+			{
+				// check zero occurence of (char)*
+				if (IsMatchSubstring(s, sIndex, regex, regexIndex + 2))
+				{
+					return true;
+				}
+
+				// check for one of more occurence of (char)*
+				while (sIndex < s.Length && (regex[regexIndex] == '.' || regex[regexIndex] == s[sIndex]))
+				{
+					if (IsMatchSubstring(s, ++sIndex, regex, regexIndex + 2))
+					{
+						return true;
+					}
+				}
+			}
+
+			// regex at current index begins with an alphanumeric char or .
+			return sIndex < s.Length && (regex[regexIndex] == '.' || regex[regexIndex] == s[sIndex]) && IsMatchSubstring(s, sIndex + 1, regex, regexIndex + 1);
+		}
+	}
+}
---
+++ EPIUnitTests/Recursion/RegexMatchingUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using EPI.Recursion;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.Recursion
+{
+	[TestClass]
+	public class RegexMatchingUnitTest
+	{
+		[TestMethod]
+		public void ESRERegexMatching()
+		{
+			RegexMatching.IsMatch("Hello World", "^Hello").Should().BeTrue();
+			RegexMatching.IsMatch("Hello World", "^Helo").Should().BeFalse();
+
+			RegexMatching.IsMatch("Hello World", "World$").Should().BeTrue();
+			RegexMatching.IsMatch("Hello World", "Hello$").Should().BeFalse();
+
+			RegexMatching.IsMatch("Hello World", "^Hello.*d$").Should().BeTrue();
+			RegexMatching.IsMatch("Hello World", "^Hello*$").Should().BeFalse();
+
+			RegexMatching.IsMatch("Hello World", "ell. W").Should().BeTrue();
+			RegexMatching.IsMatch("Hello World", "Hellw").Should().BeFalse();
+
+			RegexMatching.IsMatch("Hello World", "^Hel*o*.*").Should().BeTrue();
+			RegexMatching.IsMatch("Hello World", "Helloa*").Should().BeTrue();
+			RegexMatching.IsMatch("Hello World", "Wa*rld").Should().BeFalse();
+
+			RegexMatching.IsMatch("Hello World", "^Hello.*").Should().BeTrue();
+			RegexMatching.IsMatch("Hello World", "^Hello.* ").Should().BeTrue();
+			RegexMatching.IsMatch("Hello World", "^Hello.*A").Should().BeFalse();
+		}
+	}
+}
