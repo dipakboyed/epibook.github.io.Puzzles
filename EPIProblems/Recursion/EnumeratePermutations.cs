be38d738ebb9fd61868acb1b609e1c5fe15c0d9b
Dipak Boyed <dipakboyed@hotmail.com>
2016-07-05 21:18:36 -0700

    variant of P(n,r) permutation where r is specified

--- EPIProblems/Recursion/EnumeratePermutations.cs
+++ EPIProblems/Recursion/EnumeratePermutations.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EPI.Recursion
 {
@@ -52,6 +49,47 @@ namespace EPI.Recursion
 				result[to] = temp;
 			}
 			return result;
+		}
+
+
+		/// <summary>
+		/// Enumerate all permutations of a specific size
+		/// </summary>
+		/// <example>
+		/// size 1 for {1,2,3} results in { {1}, {2}, {3} }
+		/// size 2 for {1,2,3} results in { {1,2}, {1,3}, {2,1}, {2,3}, {3,1}, {3,2} }
+		/// size 3 for {1,2,3} results in { {1,2,3}, {1,3,2}, {2,1,3}, {2,3,1}, {3,1,2}, {3,2,1} }
+		/// </example>
+		public static List<int[]> ListAllPermutationsOfSizeK(int[] array, int k)
+		{
+			if ( k <= 0 || k > array.Length)
+			{
+				throw new ArgumentException("k must be within the range of array length");
+			}
+
+			List<int[]> result = new List<int[]>();
+			List<int> currentSet = new List<int>();
+			PermutationKHelper(array, k, 0, currentSet, result);
+			return result;
+		}
+
+		private static void PermutationKHelper(int[] array, int k, int index, List<int> currentSet, List<int[]> result)
+		{
+			// base case: add current set to result when it's size is k
+			if (k == currentSet.Count)
+			{
+				result.Add(currentSet.ToArray());
+			}
+			else
+			{
+				for (int i = index; i < array.Length; i++)
+				{
+					int[] newArray = Swap(array, index, i);
+					List<int> newSet = new List<int>(currentSet);
+					newSet.Add(newArray[index]);
+					PermutationKHelper(newArray, k, index + 1, newSet, result);
+				}
+			}
 		}
 	}
 }
--- EPIUnitTests/Recursion/EnumeratePermutationsUnitTest.cs
+++ EPIUnitTests/Recursion/EnumeratePermutationsUnitTest.cs
@@ -20,6 +20,42 @@ namespace EPI.UnitTests.Recursion
 				new[] {3,2,1},
 				new[] {3,1,2}
 			});
+
+			EnumeratePermutations.ListAllPermutationsOfSizeK(new[] { 1, 2, 3 }, 3).ShouldBeEquivalentTo(new List<int[]>()
+			{
+				new[] {1,2,3},
+				new[] {1,3,2},
+				new[] {2,1,3},
+				new[] {2,3,1},
+				new[] {3,2,1},
+				new[] {3,1,2}
+			});
+
+			EnumeratePermutations.ListAllPermutationsOfSizeK(new[] { 1, 2, 3 }, 2).ShouldBeEquivalentTo(new List<int[]>()
+			{
+				new[] {1,2},
+				new[] {1,3},
+				new[] {2,1},
+				new[] {2,3},
+				new[] {3,2},
+				new[] {3,1}
+			});
+
+			EnumeratePermutations.ListAllPermutationsOfSizeK(new[] { 1, 2, 3, 4 }, 2).ShouldBeEquivalentTo(new List<int[]>()
+			{
+				new[] {1,2},
+				new[] {1,3},
+				new[] {1,4},
+				new[] {2,1},
+				new[] {2,3},
+				new[] {2,4},
+				new[] {3,2},
+				new[] {3,1},
+				new[] {3,4},
+				new[] {4,2},
+				new[] {4,3},
+				new[] {4,1},
+			});
 		}
 	}
 }
