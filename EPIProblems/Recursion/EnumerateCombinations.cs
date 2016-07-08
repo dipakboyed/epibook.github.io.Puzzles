96126dee489ffbe337b5e2b8334f53758f9d603c
Dipak Boyed <dipakboyed@hotmail.com>
2016-07-08 00:17:41 -0700

    N choose K combinations

---
+++ EPIProblems/Recursion/EnumerateCombinations.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EPI.Recursion
+{
+	/// <summary>
+	/// Enumerate all possible combinations of a subset of length k from an array with distinct elements.
+	/// Order does not matter in the combinations.
+	/// </summary>
+	/// <example>
+	/// size 1 for {1,2,3} results in { {1}, {2}, {3} }
+	/// size 2 for {1,2,3} results in { {1,2}, {1,3}, {2,3} }
+	/// size 3 for {1,2,3} results in { {1,2,3} }
+	/// </example>
+	public static class EnumerateCombinations
+	{
+		public static List<int[]> NChooseK(int[] array, int k)
+		{
+			if (k <= 0 || k > array.Length)
+			{
+				throw new ArgumentException("k must be within the range of array length");
+			}
+
+			List<int[]> result = new List<int[]>();
+			List<int> currentSet = new List<int>();
+			CombinationsHelper(array, k, 0, currentSet, result);
+			return result;
+		}
+
+		private static void CombinationsHelper(int[] array, int k, int index, List<int> currentSet, List<int[]> result)
+		{
+			// base case: add current set to result when it's size is k
+			if (k == currentSet.Count)
+			{
+				result.Add(currentSet.ToArray());
+			}
+			else
+			{
+				for (int i = index; i < array.Length; i++)
+				{
+					List<int> newSet = new List<int>(currentSet);
+					newSet.Add(array[i]);
+					CombinationsHelper(array, k, i + 1, newSet, result);
+				}
+			}
+		}
+	}
+}
---
+++ EPIUnitTests/Recursion/EnumerateCombinationsUnitTest.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using EPI.Recursion;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.Recursion
+{
+	[TestClass]
+	public class EnumerateCombinationsUnitTest
+	{
+		[TestMethod]
+		public void ListArrayCombinations()
+		{
+			EnumerateCombinations.NChooseK(new[] { 1, 2, 3, 4, 5 }, 1).ShouldBeEquivalentTo( new List<int[]>()
+			{
+				new[] {1},
+				new[] {2},
+				new[] {3},
+				new[] {4},
+				new[] {5}
+			});
+
+			EnumerateCombinations.NChooseK(new[] { 1, 2, 3, 4, 5 }, 2).ShouldBeEquivalentTo(new List<int[]>()
+			{
+				new[] {1,2},
+				new[] {1,3},
+				new[] {1,4},
+				new[] {1,5},
+				new[] {2,3},
+				new[] {2,4},
+				new[] {2,5},
+				new[] {3,4},
+				new[] {3,5},
+				new[] {4,5},
+			});
+
+			EnumerateCombinations.NChooseK(new[] { 1, 2, 3, 4, 5 }, 3).ShouldBeEquivalentTo(new List<int[]>()
+			{
+				new[] {1,2,3},
+				new[] {1,2,4},
+				new[] {1,2,5},
+				new[] {1,3,4},
+				new[] {1,3,5},
+				new[] {1,4,5},
+				new[] {2,3,4},
+				new[] {2,3,5},
+				new[] {2,4,5},
+				new[] {3,4,5},
+			});
+
+			EnumerateCombinations.NChooseK(new[] { 1, 2, 3, 4, 5 }, 4).ShouldBeEquivalentTo(new List<int[]>()
+			{
+				new[] {1,2,3,4},
+				new[] {1,2,3,5},
+				new[] {1,2,4,5},
+				new[] {1,3,4,5},
+				new[] {2,3,4,5},
+			});
+		}
+	}
+}
