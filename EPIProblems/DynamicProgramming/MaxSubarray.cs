85ff017f6d419f2064766fe9a24f071c26e95ac0
Dipak Boyed <dipakboyed@hotmail.com>
2016-07-13 03:51:34 -0700

    Find max subarray

---
+++ EPIProblems/DynamicProgramming/MaxSubarray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace EPI.DynamicProgramming
+{
+	/// <summary>
+	/// Given an array A of n integers, find the interval indices a and b such that the
+	/// sum of A[i] between a and b is maximized.
+	/// </summary>
+	public static class MaxSubarray
+	{
+		public static Tuple<int, int> FindMaximumSubarray(int[] array)
+		{
+			// result will return range i,j such that A[i...j-1] has the max sum
+			// i is inclusive, j is not.
+			// excluding j allows returning (k,k) to represent an empty subarray
+			Tuple<int, int> result = new Tuple<int, int>(0, 0);
+			int minIndex = -1, sum = 0, minSum = 0, maxSum = int.MinValue;
+			for (int i = 0; i < array.Length; i++)
+			{
+				sum += array[i];
+				if (sum < minSum)
+				{
+					minSum = sum;
+					minIndex = i;
+				}
+				if (sum - minSum >= maxSum)
+				{
+					maxSum = sum - minSum;
+					result = new Tuple<int, int>(minIndex + 1, i + 1);
+				}
+			}
+
+			return result;
+		}
+	}
+}
---
+++ EPIUnitTests/DynamicProgramming/MaxSubarrayUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using EPI.DynamicProgramming;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.DynamicProgramming
+{
+	[TestClass]
+	public class MaxSubarrayUnitTest
+	{
+		[TestMethod]
+		public void FindMaxSubarray()
+		{
+			MaxSubarray.FindMaximumSubarray(new[] { 0, 5, -3, 7, 1, 0, -2 }).ShouldBeEquivalentTo( new Tuple<int, int>(0,6));
+			MaxSubarray.FindMaximumSubarray(new[] { 0, -5, -2, 7, 3, -3 }).ShouldBeEquivalentTo(new Tuple<int, int>(3,5));
+			MaxSubarray.FindMaximumSubarray(new[] { -1, -5, -2, -7, -3, 0 }).ShouldBeEquivalentTo(new Tuple<int, int>(5, 6));
+			MaxSubarray.FindMaximumSubarray(new[] { 0, -1, -5, -2, -7, -3 }).ShouldBeEquivalentTo(new Tuple<int, int>(6, 6));
+		}
+	}
+}
