45d8577b0b65fdab9c17803abf9d213f2d5bd41c
dipakboyed <diboyed@microsoft.com>
2016-07-24 10:32:31 -0700

    shortest path in graph

---
+++ EPIProblems/DynamicProgramming/ShortestPathInGraph.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EPI.DynamicProgramming
+{
+	/// <summary>
+	/// Given an undirected graph G having N vertices and positive weights.
+	/// Find the shortest path from vertex 1 to vertex N, or state that such path doesn’t exist.
+	/// </summary>
+	public static class ShortestPathInGraph
+	{
+		public struct Vertex
+		{
+			public char Value;
+			public Dictionary<char, int> Edges;
+		};
+
+		public static int FindLengthOfShortestPathFrom1ToN( Vertex[] graph)
+		{
+			int[] shortestPath = Enumerable.Repeat(int.MaxValue, graph.Length).ToArray();
+			shortestPath[0] = 0;
+
+			for (int i = 1; i < graph.Length; i++)
+			{
+				for (int j = 0; j < i; j++)
+				{
+					if (graph[j].Edges.ContainsKey(graph[i].Value))
+					{
+						shortestPath[i] = Math.Min(shortestPath[i], shortestPath[j] + graph[j].Edges[graph[i].Value]);
+					}
+				}
+			}
+
+			return shortestPath[graph.Length - 1];
+		}
+	}
+}
---
+++ EPIUnitTests/DynamicProgramming/ShortestPathInGraphUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using EPI.DynamicProgramming;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace EPI.UnitTests.DynamicProgramming
+{
+	[TestClass]
+	public class ShortestPathInGraphUnitTest
+	{
+		[TestMethod]
+		public void FindShortestPathFInGraphFrom1toN()
+		{
+			ShortestPathInGraph.Vertex[] graph = new[]
+			{
+				new ShortestPathInGraph.Vertex() { Value = 'a', Edges = new Dictionary<char, int>() { {'b', 1}, { 'c', 1 } }},
+				new ShortestPathInGraph.Vertex() { Value = 'b', Edges = new Dictionary<char, int>() { {'a', 1}, { 'd', 1 }, { 'e', 1 } }},
+				new ShortestPathInGraph.Vertex() { Value = 'c', Edges = new Dictionary<char, int>() { {'a', 1}, { 'd', 1 } }},
+				new ShortestPathInGraph.Vertex() { Value = 'd', Edges = new Dictionary<char, int>() { {'b', 1}, { 'c', 1 }, { 'e', 1 } }},
+				new ShortestPathInGraph.Vertex() { Value = 'e', Edges = new Dictionary<char, int>() { {'b', 1}, { 'd', 1 }, { 'f', 1 }, { 'h', 1 } }},
+				new ShortestPathInGraph.Vertex() { Value = 'f', Edges = new Dictionary<char, int>() { {'e', 1}, { 'g', 1 }, { 'i', 1 } }},
+				new ShortestPathInGraph.Vertex() { Value = 'g', Edges = new Dictionary<char, int>() { {'f', 1}, { 'h', 1 } }},
+				new ShortestPathInGraph.Vertex() { Value = 'h', Edges = new Dictionary<char, int>() { {'e', 1}, { 'g', 1 }, { 'i', 1 } }},
+				new ShortestPathInGraph.Vertex() { Value = 'i', Edges = new Dictionary<char, int>() { {'f', 1}, { 'h', 1 }, { 'j', 1 }, { 'l', 1 }, { 'm', 1 } }},
+				new ShortestPathInGraph.Vertex() { Value = 'j', Edges = new Dictionary<char, int>() { {'i', 1}, { 'k', 1 } }},
+				new ShortestPathInGraph.Vertex() { Value = 'k', Edges = new Dictionary<char, int>() { {'j', 1}, { 'l', 1 }, { 'm', 1 } }},
+				new ShortestPathInGraph.Vertex() { Value = 'l', Edges = new Dictionary<char, int>() { {'i', 1}, { 'k', 1 } }},
+				new ShortestPathInGraph.Vertex() { Value = 'm', Edges = new Dictionary<char, int>() { {'i', 1}, { 'k', 1 } }},
+			};
+
+			ShortestPathInGraph.FindLengthOfShortestPathFrom1ToN(graph).Should().Be(5);
+		}
+	}
+}
