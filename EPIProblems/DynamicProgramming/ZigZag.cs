f22c3cb0ff32b09c09a2b495ec45fc42b3968465
Dipak Boyed <dipakboyed@hotmail.com>
2016-07-24 13:02:41 -0700

    longest zig zag subsequence

---
+++ EPIProblems/DynamicProgramming/ZigZag.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace EPI.DynamicProgramming
+{
+	/// <summary>
+	/// A sequence of numbers is called a zig-zag sequence if the differences between 
+	/// successive numbers strictly alternate between positive and negative. 
+	/// The first difference (if one exists) may be either positive or negative. 
+	/// Given an array of integers, return the length of the longest subsequence
+	/// that is a zig-zag sequence.
+	/// </summary>
+	public static class ZigZag
+	{
+		public enum Order
+		{
+			Unknown,
+			Increasing,
+			Decreasing
+		}
+
+		public static int LongestZigZagSubsequence(int[] array)
+		{
+			Tuple<int, Order>[] longest = new Tuple<int, Order>[array.Length];
+			for (int i = 0; i < array.Length; i++)
+			{
+				longest[i] = new Tuple<int, Order>(int.MinValue, Order.Unknown);
+			}
+			longest[0] = new Tuple<int, Order>(1, Order.Unknown);
+
+			for (int i = 1; i < array.Length; i++)
+			{
+				for (int j = 0; j < i; j++)
+				{
+					int newLength = longest[j].Item1 + 1;
+					if (newLength > longest[i].Item1)
+					{
+						Order order = Order.Unknown;
+						if (longest[j].Item2 == Order.Unknown && array[j] != array[i])
+						{
+							order = array[j] < array[i] ? Order.Increasing : Order.Decreasing;
+						}
+						if (longest[j].Item2 == Order.Increasing && array[j] > array[i])
+						{
+							order = Order.Decreasing;
+						}
+						if (longest[j].Item2 == Order.Decreasing && array[j] < array[i])
+						{
+							order = Order.Increasing;
+						}
+
+						if (order != Order.Unknown)
+						{
+							longest[i] = new Tuple<int, Order>(newLength, order);
+						}
+					}
+				}
+			}
+
+
+			int max = 0;
+			for (int i = 0; i < longest.Length; i++)
+			{
+				if (longest[i].Item1 > max)
+				{
+					max = longest[i].Item1;
+				}
+			}
+			return max;
+		}
+	}
+}
---
+++ EPIUnitTests/DynamicProgramming/ZigZagUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using EPI.DynamicProgramming;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.DynamicProgramming
+{
+	[TestClass]
+	public class ZigZagUnitTest
+	{
+		[TestMethod]
+		public void FindLongestZigZagSubsequenceLength()
+		{
+			ZigZag.LongestZigZagSubsequence(new[] { 1, 7, 4, 9, 2, 5 }).Should().Be(6);
+			ZigZag.LongestZigZagSubsequence(new[] { 1, 17, 5, 10, 13, 15, 10, 5, 16, 8 }).Should().Be(7);
+			ZigZag.LongestZigZagSubsequence(new[] { 44}).Should().Be(1);
+			ZigZag.LongestZigZagSubsequence(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }).Should().Be(2);
+			ZigZag.LongestZigZagSubsequence(new[] { 70, 55, 13, 2, 99, 2, 80, 80, 80, 80, 100, 19, 7, 5, 5, 5, 1000, 32, 32 }).Should().Be(8);
+			ZigZag.LongestZigZagSubsequence(new[]
+			{
+				374, 40, 854, 203, 203, 156, 362, 279, 812, 955,
+				600, 947, 978, 46, 100, 953, 670, 862, 568, 188,
+				67, 669, 810, 704, 52, 861, 49, 640, 370, 908,
+				477, 245, 413, 109, 659, 401, 483, 308, 609, 120,
+				249, 22, 176, 279, 23, 22, 617, 462, 459, 244
+			}).Should().Be(36);
+		}
+	}
+}
