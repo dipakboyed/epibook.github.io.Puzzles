88f8db29b5a0efabdae542c617efaea195cc7e0b
dipakboyed <diboyed@microsoft.com>
2016-08-20 12:57:35 -0700

    knapsack

---
+++ EPIProblems/DynamicProgramming/KnapsackProblem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace EPI.DynamicProgramming
+{
+	/// <summary>
+	/// A thief's knapsack can hold at most w ounces and he breaks into a clock store. Each
+	/// clock weights i ounces and retails for v dollars. the theif must take or leave a clock.
+	/// What subset of clocks should the theif pick up to maximize the retail prices and hold up to
+	/// w ounces
+	/// </summary>
+	public static class KnapsackProblem
+	{
+		public struct Clock
+		{
+			public int Price;
+			public int Weight;
+		};
+
+		public static int Maximize(Clock[] store, int bagCapacity)
+		{
+			int[] result = Enumerable.Repeat(0, bagCapacity + 1).ToArray();
+
+			foreach (Clock clock in store)
+			{
+				for (int i = bagCapacity; i >= clock.Weight; i--)
+				{
+					result[i] = Math.Max(result[i], result[i - clock.Weight] + clock.Price);
+				}
+			}
+
+			return result[bagCapacity];
+		}
+	}
+}
---
+++ EPIUnitTests/DynamicProgramming/KnapsackProblemUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using EPI.DynamicProgramming;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.DynamicProgramming
+{
+	[TestClass]
+	public class KnapsackProblemUnitTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			KnapsackProblem.Clock[] store = new KnapsackProblem.Clock[]
+			{
+				new KnapsackProblem.Clock() { Price = 65,	Weight = 20 },
+				new KnapsackProblem.Clock() { Price = 35,	Weight = 8 },
+				new KnapsackProblem.Clock() { Price = 245,	Weight = 60 },
+				new KnapsackProblem.Clock() { Price = 195,	Weight = 55 },
+				new KnapsackProblem.Clock() { Price = 65,	Weight = 40 },
+				new KnapsackProblem.Clock() { Price = 150,	Weight = 70 },
+				new KnapsackProblem.Clock() { Price = 275,	Weight = 85 },
+				new KnapsackProblem.Clock() { Price = 155,	Weight = 25 },
+				new KnapsackProblem.Clock() { Price = 120,	Weight = 30 },
+				new KnapsackProblem.Clock() { Price = 320,	Weight = 65 },
+				new KnapsackProblem.Clock() { Price = 75,	Weight = 75 },
+				new KnapsackProblem.Clock() { Price = 40,	Weight = 10 },
+				new KnapsackProblem.Clock() { Price = 200,	Weight = 95 },
+				new KnapsackProblem.Clock() { Price = 100,	Weight = 50 },
+				new KnapsackProblem.Clock() { Price = 220,	Weight = 40 },
+				new KnapsackProblem.Clock() { Price = 99,	Weight = 10 }
+			};
+			KnapsackProblem.Maximize(store, 130).Should().Be(695);
+		}
+	}
+}
