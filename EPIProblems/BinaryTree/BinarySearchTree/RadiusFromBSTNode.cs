cb5d961334e067f18dc8d2917a873790210689c8
Dipak Boyed <dipakboyed@hotmail.com>
2017-03-05 13:23:01 -0800

    BST : enumerate nodes within given radius

---
+++ EPIProblems/BinaryTree/BinarySearchTree/RadiusFromBSTNode.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+namespace EPI.BinaryTree.BinarySearchTree
+{
+	public static class RadiusFromBSTNode
+	{
+		public static List<int> FindAllNodesWithinRadius(BinaryTreeNode<int> node, int val, int radius)
+		{
+			List<int> result = new List<int>();
+			// iterate BST and put all ancestors in a stack
+			Stack<BinaryTreeNode<int>> stack = new Stack<BinaryTreeNode<int>>();
+			bool found = AddPathToNode(node, val, stack);
+
+			if (found)
+			{
+				int dist = 0;
+				HashSet<BinaryTreeNode<int>> visited = new HashSet<BinaryTreeNode<int>>();
+				while (dist <= radius && stack.Count > 0)
+				{
+					BinaryTreeNode<int> curr = stack.Pop();
+					FindNodesWithinSubTree(curr, radius - dist, visited, result);
+					dist++;
+				}
+			}
+			return result;
+		}
+
+		static void FindNodesWithinSubTree(BinaryTreeNode<int> node, int r, HashSet<BinaryTreeNode<int>> visited, List<int> result)
+		{
+			if (r >= 0 && node != null && !visited.Contains(node))
+			{
+				visited.Add(node);
+				result.Add(node.Value);
+
+				FindNodesWithinSubTree(node.Left, r - 1, visited, result);
+				FindNodesWithinSubTree(node.Right, r - 1, visited, result);
+			}
+		}
+
+		static bool AddPathToNode(BinaryTreeNode<int> node, int val, Stack<BinaryTreeNode<int>> stack)
+		{
+			if (node != null)
+			{
+				stack.Push(node);
+				if (node.Value == val)
+				{
+					return true;
+				}
+				else if (node.Value > val)
+				{
+					return AddPathToNode(node.Left, val, stack);
+				}
+				else
+				{
+					return AddPathToNode(node.Right, val, stack);
+				}
+			}
+			return false;
+		}
+	}
+}
---
+++ EPIUnitTests/BinaryTree/BinarySearchTree/RadiusFromBSTNodeUnitTest.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using EPI.BinaryTree;
+using EPI.BinaryTree.BinarySearchTree;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPI.UnitTests.BinaryTree.BinarySearchTree
+{
+	[TestClass]
+	public class RadiusFromBSTNodeUnitTest
+	{
+		private BinaryTree<int> bst = new BinaryTree<int>()
+		{
+			Root = new BinaryTreeNode<int>(100)
+			{
+				Left = new BinaryTreeNode<int>(50)
+				{
+					Left = new BinaryTreeNode<int>(25)
+					{
+						Left = new BinaryTreeNode<int>(12),
+						Right = new BinaryTreeNode<int>(37)
+					},
+					Right = new BinaryTreeNode<int>(75)
+					{
+						Left = new BinaryTreeNode<int>(63),
+						Right = new BinaryTreeNode<int>(87)
+					}
+				},
+				Right = new BinaryTreeNode<int>(150)
+				{
+					Left = new BinaryTreeNode<int>(125)
+					{
+						Left = new BinaryTreeNode<int>(112),
+						Right = new BinaryTreeNode<int>(137)
+					},
+					Right = new BinaryTreeNode<int>(175)
+					{
+						Left = new BinaryTreeNode<int>(163),
+						Right = new BinaryTreeNode<int>(187)
+					}
+				}
+			}
+		};
+
+		[TestMethod]
+		public void FindAllNodesInRadius()
+		{
+			RadiusFromBSTNode.FindAllNodesWithinRadius(bst.Root, 100, 0).ShouldAllBeEquivalentTo(new List<int>() { 100 });
+			RadiusFromBSTNode.FindAllNodesWithinRadius(bst.Root, 100, 1).ShouldAllBeEquivalentTo(new List<int>() { 100, 50, 150 });
+			RadiusFromBSTNode.FindAllNodesWithinRadius(bst.Root, 100, 2).ShouldAllBeEquivalentTo(new List<int>() { 100, 50, 150, 25, 75, 125, 175 });
+			RadiusFromBSTNode.FindAllNodesWithinRadius(bst.Root, 100, -1).ShouldAllBeEquivalentTo(new List<int>() { });
+			RadiusFromBSTNode.FindAllNodesWithinRadius(bst.Root, 1, 0).ShouldAllBeEquivalentTo(new List<int>() { });
+
+			RadiusFromBSTNode.FindAllNodesWithinRadius(bst.Root, 50, 0).ShouldAllBeEquivalentTo(new List<int>() { 50 });
+			RadiusFromBSTNode.FindAllNodesWithinRadius(bst.Root, 50, 1).ShouldAllBeEquivalentTo(new List<int>() { 50, 25, 75, 100 });
+			RadiusFromBSTNode.FindAllNodesWithinRadius(bst.Root, 50, 2).ShouldAllBeEquivalentTo(new List<int>() { 50, 25, 12, 37, 75, 63, 87, 100, 150 });
+			RadiusFromBSTNode.FindAllNodesWithinRadius(bst.Root, 50, 3).ShouldAllBeEquivalentTo(new List<int>() { 50, 25, 12, 37, 75, 63, 87, 100, 150, 125, 175 });
+			RadiusFromBSTNode.FindAllNodesWithinRadius(bst.Root, 50, 4).ShouldAllBeEquivalentTo(new List<int>() { 50, 25, 12, 37, 75, 63, 87, 100, 150, 125, 112, 137, 175, 163, 187 });
+			RadiusFromBSTNode.FindAllNodesWithinRadius(bst.Root, 50, 5).ShouldAllBeEquivalentTo(new List<int>() { 50, 25, 12, 37, 75, 63, 87, 100, 150, 125, 112, 137, 175, 163, 187 });
+
+			RadiusFromBSTNode.FindAllNodesWithinRadius(bst.Root, 87, 1).ShouldAllBeEquivalentTo(new List<int>() { 87, 75 });
+			RadiusFromBSTNode.FindAllNodesWithinRadius(bst.Root, 87, 2).ShouldAllBeEquivalentTo(new List<int>() { 87, 75, 63, 50 });
+			RadiusFromBSTNode.FindAllNodesWithinRadius(bst.Root, 63, 3).ShouldAllBeEquivalentTo(new List<int>() { 63, 75, 87, 50, 25, 100 });
+			RadiusFromBSTNode.FindAllNodesWithinRadius(bst.Root, 163, 4).ShouldAllBeEquivalentTo(new List<int>() { 163, 175, 187, 150, 125, 112, 137, 100, 50 });
+		}
+	}
+}
